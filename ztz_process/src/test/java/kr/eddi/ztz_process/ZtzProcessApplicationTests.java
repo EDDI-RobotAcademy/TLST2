5e19c6698265632a480a44be81d01cdecfa12e9d
Karam Kim <kim.ram454@gmail.com>
2022-11-30 20:06:51 +0900

    init: [김가람] 팀프로젝트 초기화

---
+++ ztz_process/src/test/java/kr/eddi/ztz_process/ZtzProcessApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.eddi.ztz_process;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZtzProcessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
