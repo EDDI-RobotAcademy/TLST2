3fd072f335fcd831590c61e088714564c901d5bb
dobbymun <tearofcake@gmail.com>
2022-12-01 21:44:37 +0900

    feat:[박진주] 공통으로 사용할 수 있는 버튼 컴포넌트 [ZTZ-WEB-UI3]

---
+++ ztz_web/src/components/common/CommonButtonWhite.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-btn
+    class="ma-2"
+    color="black"
+    outlined
+    elevation="2"
+    v-on="$listeners"
+    v-bind="$attrs"
+    v-if="!isMobile()">
+   <v-icon small>
+     {{iconName}}
+   </v-icon>
+    <span style="width: 5px" v-if="btnName"></span>
+    {{btnName}}
+  </v-btn>
+
+<!--
+ isMobile이 true(모바일화면인 경우-> 화면사이즈 작음) btnName만 보임
+ -->
+  <v-btn
+      class="ma-2"
+      color="blue lighten-1"
+      outlined
+      elevation="2"
+      v-on="$listeners"
+      v-bind="$attrs"
+      v-else>
+    <span style="width: 3px" v-if="btnName"></span>
+    {{btnName}}
+  </v-btn>
+</template>
+
+<script>
+import commonMixins from '@/mixins/CommonMixins'
+
+//mixins를 통해 isMobile()이 동작하고 휴대폰인지 pc인지를 판정
+
+export default {
+  name: "CommonButton",
+  mixins: [commonMixins],
+  props: {
+    iconName: String,
+    btnName: String,
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- ztz_web/src/main.js
+++ ztz_web/src/main.js
@@ -3,8 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import CommonButtonWhite from "@/components/common/CommonButtonWhite";
+import CommonButtonBlack from "@/components/common/CommonButtonBlack";
 
 Vue.config.productionTip = false
+
+
+Vue.component('CommonButtonBlack', CommonButtonBlack)
+Vue.component('CommonButtonWhite', CommonButtonWhite)
 
 new Vue({
   router,
