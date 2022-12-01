201d0b4778fe91df41628d0ee4556c21226f23ce
dobbymun <tearofcake@gmail.com>
2022-12-01 21:53:23 +0900

    feat:[박진주] 회원가입 버튼 클릭시 사용자가 회원가입을 위해입력할 양식이 나오는 UI[ZTZ-WEB-UI4]

---
+++ ztz_web/src/views/TestView.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container>
+    <router-link to="/sign-up">회원가입</router-link> |
+  </v-container>
+
+</template>
+
+<script>
+
+  export default {
+    name: 'HomeView',
+
+    components: {
+
+    },
+  }
+</script>
