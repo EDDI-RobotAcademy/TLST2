1802ef41b88c7982b81f69f4f77c6ffe5267584e
dobbymun <tearofcake@gmail.com>
2022-12-02 21:02:28 +0900

    feat:[박진주] 사용자의 이메일, 비밀번호를 입력받아 로그인시켜주는 로그인시스템 [ZTZ-PROCESS3]

--- ztz_process/src/main/java/kr/eddi/ztz_process/service/member/MemberService.java
+++ ztz_process/src/main/java/kr/eddi/ztz_process/service/member/MemberService.java
@@ -1,11 +1,13 @@
 package kr.eddi.ztz_process.service.member;
 
+import kr.eddi.ztz_process.service.member.request.MemberLoginRequest;
 import kr.eddi.ztz_process.service.member.request.MemberRegisterRequest;
 
 
 public interface MemberService {
     Boolean signUp(MemberRegisterRequest request);
     Boolean emailValidation(String email);
+    String signIn(MemberLoginRequest request);
 
 
 }
--- ztz_process/src/main/java/kr/eddi/ztz_process/service/member/MemberServiceImpl.java
+++ ztz_process/src/main/java/kr/eddi/ztz_process/service/member/MemberServiceImpl.java
@@ -5,12 +5,15 @@ import kr.eddi.ztz_process.entity.member.BasicAuthentication;
 import kr.eddi.ztz_process.entity.member.Member;
 import kr.eddi.ztz_process.repository.member.AuthenticationRepository;
 import kr.eddi.ztz_process.repository.member.MemberRepository;
+import kr.eddi.ztz_process.service.member.request.MemberLoginRequest;
 import kr.eddi.ztz_process.service.member.request.MemberRegisterRequest;
+import kr.eddi.ztz_process.service.security.RedisService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
+import java.util.UUID;
 
 @Slf4j
 @Service
@@ -21,6 +24,9 @@ public class MemberServiceImpl implements MemberService {
 
     @Autowired
     private AuthenticationRepository authenticationRepository;
+
+    @Autowired
+    private RedisService redisService;
 
 
     @Override
@@ -44,4 +50,38 @@ public class MemberServiceImpl implements MemberService {
 
         return true;
     }
+
+    @Override
+    public String signIn(MemberLoginRequest request) {
+        String email = request.getEmail();
+        Optional<Member> maybeMember = memberRepository.findByEmail(email);
+
+        if (maybeMember.isPresent()) {
+            Member member = maybeMember.get();
+
+            log.info("member email: " + member.getEmail());
+            log.info("request email: " + request.getEmail());
+            log.info("request password: " + request.getPassword());
+
+            if (!member.isRightPassword(request.getPassword())) {
+                // 패스워드가 잘못 된 경우
+                throw new RuntimeException("아이디 또는 패스워드가 잘못되었습니다");
+            }
+
+            UUID userToken = UUID.randomUUID();
+
+            redisService.deleteByKey(userToken.toString());
+            redisService.setKeyAndValue(userToken.toString(), member.getId());
+
+            log.info("유저토큰: "+userToken.toString());
+            log.info("레디스 유저토큰으로 멤버아이디 구하기: "+ redisService.getValueByKey(userToken.toString()));
+            log.info("유저토큰:"+userToken.toString() +" 멤버아이디:"+ member.getId());
+
+            return userToken.toString();
+        }
+
+        // 이메일이 가입되어 있지 않은 경우
+        throw new RuntimeException("아이디 또는 패스워드가 잘못되었습니다");
+    }
+
 }
