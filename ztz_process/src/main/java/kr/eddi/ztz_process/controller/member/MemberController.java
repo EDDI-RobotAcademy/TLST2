527518326ea53bf7846162f18496e4b4c0d1809c
dobbymun <tearofcake@gmail.com>
2022-12-02 21:02:28 +0900

    feat:[박진주] 사용자의 이메일, 비밀번호를 입력받아 로그인시켜주는 로그인시스템 [ZTZ-PROCESS3]

--- ztz_process/src/main/java/kr/eddi/ztz_process/controller/member/MemberController.java
+++ ztz_process/src/main/java/kr/eddi/ztz_process/controller/member/MemberController.java
@@ -1,5 +1,6 @@
 package kr.eddi.ztz_process.controller.member;
 
+import kr.eddi.ztz_process.controller.member.form.MemberLoginForm;
 import kr.eddi.ztz_process.controller.member.form.MemberRegisterForm;
 import kr.eddi.ztz_process.service.member.MemberService;
 import lombok.extern.slf4j.Slf4j;
@@ -30,5 +31,11 @@ public class MemberController {
         return service.signUp(form.toMemberRegisterRequest());
     }
 
+    @PostMapping("/sign-in")
+    public String signIn(@RequestBody MemberLoginForm form) {
+        log.info("signIn: " + form);
+
+        return service.signIn(form.toLoginRequest());
+    }
 
 }
