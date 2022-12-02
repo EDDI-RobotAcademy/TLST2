eb76cae56e48061f812290d815ff02daf64616d2
dobbymun <tearofcake@gmail.com>
2022-12-02 20:27:53 +0900

    feat:[박진주] 회원가입 버튼을 눌렀을 때 나오는 회원가입 UI [ZTZ-WEB-UI4]

---
+++ ztz_web/src/views/TestView.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-container>
+    <router-link to="/sign-up">회원가입</router-link> |
+  </v-container>
+
+</template>
+
+<script>
+
+  export default {
+    name: 'HomeView',
+    components: {
+
+    },
+    methods:{
+      async logout() {
+        console.log("로그아웃합니다")
+        this.$store.commit('IS_AUTHENTICATED', false)
+        localStorage.removeItem("userInfo")
+        this.$cookies.remove('user')
+        alert('로그아웃 되었습니다.')
+        await this.$router.push({name:'SignInView'})
+      }
+    }
+  }
+</script>
