18cb90458539812e07b37e800e999e61c229906f
dobbymun <tearofcake@gmail.com>
2022-12-02 20:27:53 +0900

    feat:[박진주] 회원가입 버튼을 눌렀을 때 나오는 회원가입 UI [ZTZ-WEB-UI4]

--- ztz_web/src/router/index.js
+++ ztz_web/src/router/index.js
@@ -1,14 +1,21 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
+import SignUpView from "@/views/account/SignUpView";
+import TestView from "@/views/TestView";
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: '/test',
+    name: 'test',
+    component: TestView
+  },
+  {
+    path: '/sign-up',
+    name: 'SignUpView',
+    component: SignUpView
   },
   {
     path: '/about',
---
+++ ztz_web/src/views/account/SignUpView.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-container>
+    <sign-up-form @submit="onSubmit"/>
+  </v-container>
+</template>
+
+<script>
+
+import axios from "axios";
+import SignUpForm from "@/components/account/SignUpForm";
+
+export default {
+  name: "SignUpView",
+  components: {
+    SignUpForm
+  },
+  methods: {
+     onSubmit (payload) {
+      const { email, password, username, birthdate} = payload;
+
+      axios.post("http://localhost:7777/ztz/member/sign-up", {
+        email, password, username, birthdate
+      })
+      .then((res) => {
+        alert("회원 가입이 완료되었습니다.")
+        console.log(res)
+        this.$router.push("/sign-in")
+      })
+      .catch((res) => {
+        alert(res.response.data.message)
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
