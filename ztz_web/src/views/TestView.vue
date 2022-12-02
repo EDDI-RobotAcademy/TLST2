798886a845512fc00a411369fc52b126673c4026
dobbymun <tearofcake@gmail.com>
2022-12-02 21:04:04 +0900

    feat:[박진주] 로그아웃버튼을 클릭 시 로그아웃되는 기능 UI [ZTZ-WEB-UI 9]

--- ztz_web/src/views/TestView.vue
+++ ztz_web/src/views/TestView.vue
@@ -2,6 +2,7 @@
   <v-container>
     <router-link to="/sign-up">회원가입</router-link> |
     <router-link to="/sign-in">로그인</router-link> |
+    <button-white btn-name="로그아웃" @click="logout"/>
   </v-container>
 
 </template>
@@ -14,6 +15,14 @@
 
     },
     methods:{
+      async logout() {
+        console.log("로그아웃합니다")
+        this.$store.commit('IS_AUTHENTICATED', false)
+        localStorage.removeItem("userInfo")
+        this.$cookies.remove('user')
+        alert('로그아웃 되었습니다.')
+        await this.$router.push({name:'SignInView'})
+      }
     }
   }
 </script>
