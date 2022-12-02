3f7a9d11a15120d635bedd52bbdd7e621dd53339
dobbymun <tearofcake@gmail.com>
2022-12-02 20:38:48 +0900

    feat:[박진주] 로그인 버튼을 눌렀을 때 나오는 로그인 UI [ZTZ-WEB-UI 5]

--- ztz_web/src/views/TestView.vue
+++ ztz_web/src/views/TestView.vue
@@ -1,6 +1,7 @@
 <template>
   <v-container>
     <router-link to="/sign-up">회원가입</router-link> |
+    <router-link to="/sign-in">로그인</router-link> |
   </v-container>
 
 </template>
@@ -13,14 +14,6 @@
 
     },
     methods:{
-      async logout() {
-        console.log("로그아웃합니다")
-        this.$store.commit('IS_AUTHENTICATED', false)
-        localStorage.removeItem("userInfo")
-        this.$cookies.remove('user')
-        alert('로그아웃 되었습니다.')
-        await this.$router.push({name:'SignInView'})
-      }
     }
   }
 </script>
