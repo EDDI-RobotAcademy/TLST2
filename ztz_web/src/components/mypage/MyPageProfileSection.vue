bf051c3178e69033e2a059416b4ae031ccb24c55
tonowoods <tono.woods@gmail.com>
2022-12-06 20:10:10 +0900

    feat:[임보경] 로그인한 사용자가 마이페이지 메뉴를 누르면 이동하는 마이페이지 메인 UI[ZTZ-WEB-UI16]

---
+++ ztz_web/src/components/mypage/MyPageProfileSection.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="profile-section">
+
+    <div class="management-box  first-box">
+      <p class="name head-text">홍길동님</p>
+      <p class="email">irigeori@josun.com</p>
+    </div>
+
+    <nav>
+      <ul class="sub-nav">
+        <li><a href="#">회원정보변경</a></li>
+        <li><a href="#">배송지관리</a></li>
+        <li><a href="#">주문관리</a></li>
+        <li><a href="#">리뷰관리</a></li>
+        <li><a href="#">예약관리</a></li>
+        <li><a href="#">회원탈퇴</a></li>
+      </ul>
+    </nav>
+
+  </section>
+</template>
+
+<script>
+export default {
+  name: "MyPageProfileSection"
+}
+</script>
+
+<style scoped>
+
+</style>
