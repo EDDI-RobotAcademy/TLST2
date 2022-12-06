ed2100b35d7991af515b710b0390535182e7a2ef
tonowoods <tono.woods@gmail.com>
2022-12-06 17:32:18 +0900

    feat: 상품 entity 생성 [ZTZ-PROCESS7]

---
+++ ztz_process/src/main/java/kr/eddi/ztz_process/entity/products/Product.java
@@ -0,0 +1,36 @@
+package kr.eddi.ztz_process.entity.products;
+
+import lombok.Data;
+import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long productNo;
+
+    @Column(nullable = false)
+    private String brand;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private Long price;
+
+    @Embedded
+    private Classification classification;
+    @Embedded
+    private ProductInfo productInfo;
+
+    public Product(String brand, String name, Long price, Classification classification,ProductInfo productInfo) {
+        this.brand = brand;
+        this.name = name;
+        this.price = price;
+        this.classification = classification;
+        this.productInfo = productInfo;
+    }
+
+
+}
