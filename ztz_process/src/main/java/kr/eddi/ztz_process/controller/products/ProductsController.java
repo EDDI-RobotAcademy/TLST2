1fc2c0a8912f0435d92902a8c2a17b262d4b5ce5
tonowoods <tono.woods@gmail.com>
2022-12-06 17:41:03 +0900

    feat: [임보경] 프론트에서 요청한 특정지역 상품만 전달하는 시스템 [ZTZ-PROCESS8]

--- ztz_process/src/main/java/kr/eddi/ztz_process/controller/products/ProductsController.java
+++ ztz_process/src/main/java/kr/eddi/ztz_process/controller/products/ProductsController.java
@@ -1,5 +1,6 @@
 package kr.eddi.ztz_process.controller.products;
 
+import kr.eddi.ztz_process.entity.products.Local;
 import kr.eddi.ztz_process.entity.products.Product;
 import kr.eddi.ztz_process.service.products.ProductsService;
 import lombok.extern.slf4j.Slf4j;
@@ -7,16 +8,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+
 @Slf4j
 @RestController
 @RequestMapping("/products")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class ProductsController {
+
     @Autowired
     ProductsService productsService;
+
     @GetMapping(path = "/list")
     public List<Product> productsList() {
         return productsService.list();
     }
+    @GetMapping(path = "/list/{localName}")
+    public List<Product> productsList(@PathVariable("localName") String localName) {
+        String tmp = localName;
+        Local filterLocal = Local.valueOfLocalName(tmp);
 
+        return productsService.list(filterLocal);
+    }
 }
