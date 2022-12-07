0121a8cc569c101704be9cb71fce58b1209bbbf5
tonowoods <tono.woods@gmail.com>
2022-12-08 00:17:46 +0900

    feat:[임보경] axios를 이용해 서버로 회원탈퇴를 요청하는 기능 추가 [ZTZ-WEB-UI16]

--- ztz_web/src/store/actions.js
+++ ztz_web/src/store/actions.js
@@ -1,6 +1,6 @@
 import {
     REQUEST_PRODUCTS_LIST_FROM_SPRING,
-    REQUEST_FILTERED_PRODUCT_FROM_SPRING
+    REQUEST_FILTERED_PRODUCT_FROM_SPRING, RESPONSE_MY_REQUEST
 } from './mutation-types'
 
 // npm install axios --save-dev
@@ -15,11 +15,17 @@ export default {
             })
     },
     reqFilteredProductsFromSpring ({ commit }, localName) {
-        console.log('해당 지역 상품만 가져오기')
-
         return axios.get(`http://localhost:7777/products/list/${localName}`)
             .then((res) => {
                 commit(REQUEST_FILTERED_PRODUCT_FROM_SPRING, res.data)
             })
     },
+    reqDeleteMemberToSpring ({ commit }, token) {
+        return axios.delete('http://localhost:7777/ztz/member/withdrawal', {
+            headers: { token : token }
+        }).then((res) => {
+                commit(RESPONSE_MY_REQUEST, res.data)
+            })
+    },
+
 }
