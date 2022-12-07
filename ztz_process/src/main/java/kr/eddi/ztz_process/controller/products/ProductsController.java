b71094522571b2836fcbd536ec59d79680d57be4
tonowoods <tono.woods@gmail.com>
2022-12-07 20:55:54 +0900

    feat: [임보경] products 누락된 코드 및 오타 수정

--- ztz_process/src/main/java/kr/eddi/ztz_process/controller/products/ProductsController.java
+++ ztz_process/src/main/java/kr/eddi/ztz_process/controller/products/ProductsController.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @Slf4j
 @RestController
-@RequestMapping("/products")
+@RequestMapping("ztz/products")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class ProductsController {
 
