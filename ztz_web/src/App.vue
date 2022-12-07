c0e332cb386aada524a31d348fd43b33a57606e8
Park-Keun-Soo <84124946+Park-Keun-Soo@users.noreply.github.com>
2022-12-07 22:25:31 +0900

    feat [박근수] : 메인페이지에서 공통으로 사용할 수 있는 Header [ZTZ-WEB-UI1]

--- ztz_web/src/App.vue
+++ ztz_web/src/App.vue
@@ -1,55 +1,27 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
+  <div id="app">
     <v-main>
       <router-view/>
     </v-main>
-  </v-app>
+  </div>
 </template>
 
 <script>
 
 export default {
-  name: 'App',
-
-  data: () => ({
-    //
-  }),
-};
+  name: "App",
+  components: {
+  },
+  computed: {
+  }
+}
 </script>
+
+<style scoped>
+
+#app {
+  height: 100%;
+  position: relative;
+  padding-bottom: 170px;
+}
+</style>
---
+++ ztz_web/src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-container>
+    <div id="home">
+      <MainHeader/>
+      <MainFooter/>
+    </div>
+  </v-container>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeView',
+
+  components: {
+
+  },
+}
+</script>
