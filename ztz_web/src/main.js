89846c0d26d00b47c352bade0767b727ae0f4f5c
Karam Kim <kim.ram454@gmail.com>
2022-12-08 23:54:04 +0900

    feat:[김가람] 특정한 권한없이 모든 사용자가 상품 상세 페이지 내에서 볼 수 있는 상품 리뷰 UI[ZTZ-WEB-UI8]

--- ztz_web/src/main.js
+++ ztz_web/src/main.js
@@ -5,6 +5,7 @@ import store from './store'
 import vuetify from './plugins/vuetify'
 import ButtonGreen from "@/components/common/ButtonGreen";
 import ButtonWhite from "@/components/common/ButtonWhite";
+import VuePaginate from 'vue-paginate'
 
 Vue.config.productionTip = false
 
@@ -14,6 +15,7 @@ import "@/css/mypage.css";
 import MainHeader from "@/components/common/header/MainHeader";
 import MainFooter from "@/components/common/footer/MainFooter";
 
+Vue.use(VuePaginate)
 Vue.component('MainHeader', MainHeader)  // 글로벌 컴포넌트
 Vue.component('MainFooter', MainFooter)
 Vue.component('ButtonGreen', ButtonGreen)
