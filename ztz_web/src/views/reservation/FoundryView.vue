3a41db1ad1c7e5b747ea888ef8e41e557c61fa39
tonowoods <tono.woods@gmail.com>
2022-12-09 20:24:49 +0900

    feat:[임보경] 특정한 권한이 필요없이 접근 가능한 양조장 메인 UI[ZTZ-WEB-UI22]

---
+++ ztz_web/src/views/reservation/FoundryView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="wrap foundryView">
+    <p class="menu-head-text">TOUR + EXPERIENCE</p>
+    <foundry-list />
+  </div>
+</template>
+<script>
+import "@/css/initialization.css";
+import "@/css/foundry.css";
+import FoundryList from "@/components/reservation/FoundryList.vue";
+
+export default {
+  name: "FoundryView",
+  components: {FoundryList},
+};
+</script>
