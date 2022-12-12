3a08a943766e79a8db0ca4d1d6e7cdb69115a3b6
choi jaemin <foertonw5@naver.com>
2022-12-12 17:27:39 +0900

    feat:[최재민] 프론트에서 요청한 맴버 프로필 정보 리턴 하는 시스템[ZTZ-PROCESS29]

--- ztz_process/src/main/java/kr/eddi/ztz_process/repository/member/MemberRepository.java
+++ ztz_process/src/main/java/kr/eddi/ztz_process/repository/member/MemberRepository.java
@@ -2,6 +2,7 @@ package kr.eddi.ztz_process.repository.member;
 
 
 import kr.eddi.ztz_process.entity.member.Member;
+import kr.eddi.ztz_process.entity.member.MemberProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -14,4 +15,7 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     @Query("select m from Member m join fetch m.authentications where m.id = :id")
     Member findByMemberId(Long id);
+
+    @Query("select mf from MemberProfile mf join fetch mf.member where mf.id= :memberId")
+    MemberProfile findProfileByMemberId(Long memberId);
 }
