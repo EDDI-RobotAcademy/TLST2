157bdc864aab393c0012d2cdaae61d705e729284
choi jaemin <foertonw5@naver.com>
2022-12-12 17:44:23 +0900

    feat:[최재민] 로그인한 사용자가 주문하기 버튼 클릭 시 주문 정보를 확인, 입력 할 수 있는 주문 페이지UI 전면 수정[ZTZ-WEB-UI38]

--- ztz_web/src/components/order/OrderInfoForm.vue
+++ ztz_web/src/components/order/OrderInfoForm.vue
@@ -1,187 +1,104 @@
 <template>
   <div class="white" style="font-family: Arial">
-    <h2>결제하기</h2>
-    <v-col style="margin-top: 20px">
-      <p style="width: 800px; margin-bottom: 15px;border-top: 2px solid black"></p>
+    <v-container>
+      <h3>주문/결제 하기</h3>
+      <v-divider class="mt-3 mb-3"/>
+      <p align="left">수령자 정보</p>
 
-      <v-btn @click="orderMemberInfoVisible" plain>
-        주문정보 확인하기
-        <p style="width: 600px"></p>
-        <v-icon v-if="ShowDirection">mdi-chevron-down</v-icon>
-        <v-icon v-else>mdi-chevron-up</v-icon>
-      </v-btn>
+      <v-card class="member-info-card" outlined>
 
-      <div v-if="MemberInfoShow" style="margin-top: 50px; margin-left: 25px">
-        <template>
-          <v-row justify="left" >
-            <p style="padding-top: 9px; width: 150px; font-size: 13px">이름</p>
-            <p style="width: 600px">
-              <v-text-field outlined dense="true" style="font-size: 13px"></v-text-field>
-            </p>
-          </v-row>
+        <v-row class="member-row" >
+          <p class="member-tool" align="left">수령인</p>
+          <p class="member-info">{{ this.$store.state.resMember.username }}</p>
+        </v-row>
+        <v-divider class="mt-1 mb-3"/>
 
-          <v-row justify="left" >
-            <p style="padding-top: 9px; width: 150px; font-size: 13px">이메일</p>
-            <p style="width: 250px">
-              <v-text-field outlined dense="true" style="font-size: 13px"></v-text-field>
-            </p>
-            <p style="margin: 5px">@</p>
-            <p style="width: 330px;">
-              <v-select :items="PortalSites" outlined dense="true" style="font-size: 13px;"></v-select>
-            </p>
-          </v-row>
+        <v-row class="member-row" >
+          <p class="member-tool" align="left">전화번호</p>
+          <p class="member-info">{{ this.$store.state.resMemberProfile.phoneNumber }}</p>
+        </v-row>
+        <v-divider class="mt-1 mb-3"/>
 
-          <v-row justify="left" >
-            <p style="padding-top: 9px; width: 150px; font-size: 13px">전화번호</p>
-            <p style="width: 250px">
-              <v-select :items="LocalNumber" outlined dense="true" style="font-size: 13px;"></v-select>
-            </p>
-            <p style="margin: 9px">-</p>
-            <p style="width: 330px;">
-            <v-text-field label=" '-'없이 휴대폰 번호 입력" outlined dense="true" style="font-size: 13px;"></v-text-field>
-            </p>
-          </v-row>
-        </template>
-      </div>
-
-      <p style="width: 800px; margin-bottom: 15px;border-top: 2px solid black"></p>
-      <v-row>
-        <p style="margin-top: 20px; margin-left: 30px">배송지 정보</p>
-        <v-btn style="margin-top: 20px; margin-left: 530px" small>주문 고객과 동일</v-btn>
-      </v-row>
-
-      <div style="margin-top: 50px; margin-left: 25px">
-        <v-row justify="left" >
-          <p style="padding-top: 9px; width: 150px; font-size: 13px">받는분</p>
-          <p style="width: 600px">
-            <v-text-field outlined dense="true" style="font-size: 13px"></v-text-field>
+        <v-row class="member-row" >
+          <p class="member-tool" align="left">배송지</p>
+          <p class="member-info">
+            {{ this.$store.state.resMemberProfile.address.city }}
+            {{ this.$store.state.resMemberProfile.address.street }}
+            {{ this.$store.state.resMemberProfile.address.addressDetail }}
+            ({{ this.$store.state.resMemberProfile.address.zipcode }}) |
+            <V-btn @click="setDestinationToAddress" v-if="setDestination" style="height: 17px; font-size: 12px" outlined>주소지를 배송지로</V-btn>
+            <V-btn @click="setDestinationToAddress" v-else style="height: 17px; font-size: 12px" outlined>배송지 입력하기</V-btn>
           </p>
         </v-row>
+        <v-divider class="mt-1 mb-3"/>
 
-        <v-row justify="left" >
-          <p style="padding-top: 9px; width: 150px; font-size: 13px">전화번호</p>
-          <p style="width: 250px">
-            <v-select :items="LocalNumber" outlined dense="true" style="font-size: 13px;"></v-select>
-          </p>
-          <p style="margin: 9px">-</p>
-          <p style="width: 330px;">
-            <v-text-field label=" '-'없이 휴대폰 번호 입력" outlined dense="true" style="font-size: 13px;"></v-text-field>
-          </p>
+        <v-row class="member-row">
+          <p class="member-tool mt-3" align="left">배송시 요청 사항</p>
+          <v-select  v-bind:items="request" v-model="selectedRequest" class="member-request-select" dense outlined/>
         </v-row>
 
-          <v-row justify="left">
-            <p style="padding-top: 9px; width: 150px; font-size: 13px">주소</p>
-            <div class="d-flex">
-              <v-text-field v-model="zipcode" outlined dense disabled style="font-size: 13px;" required/>
-            </div>
-            <v-btn style="font-size: 13px; height: 40px; margin-left: 8px" @click="callDaumAddressApi" outlined dense>우편번호 찾기</v-btn>
-          </v-row>
+        <v-text-field v-if="directlyInput" class="member-request-direct" v-model="directRequest" outlined dense/>
+      </v-card>
 
-        <div class="d-flex" style="margin-left: 138px; width: 600px">
-          <v-text-field v-model="city" outlined dense disabled style="font-size: 13px;" required/>
-        </div>
-
-        <div class="d-flex" style="margin-left: 138px; width: 600px">
-          <v-text-field v-model="street" outlined dense disabled style="font-size: 13px;" required/>
-        </div>
-
-        <div class="d-flex" style="margin-left: 138px; width: 600px">
-          <v-text-field v-model="addressDetail" outlined dense style="font-size: 13px;" required/>
-        </div>
-
-      </div>
-    </v-col>
-    <v-card outlined width="775px" style="margin-bottom: 30px">
-      <p style="padding: 20px">
-        배송요청사항
+      <v-divider class="mt-3 mb-3"/>
+      <p align="left">
+        배송지 정보
       </p>
-      <v-select :items="RequestItems" v-model="Request" outlined dense="true" style="font-size: 13px; margin: 30px">
 
-      </v-select>
-    </v-card>
+      <v-card v-if="setAddress" outlined style="padding: 5px">
+        <p style="font-size: 12px; padding: 2px">
+          {{ this.$store.state.resMemberProfile.address.city }}
+          {{ this.$store.state.resMemberProfile.address.street }}
+          {{ this.$store.state.resMemberProfile.address.addressDetail }}
+          ({{ this.$store.state.resMemberProfile.address.zipcode }})
+        </p>
+      </v-card>
 
-<!--    주문상품-->
-    <p style="width: 800px; margin-bottom: 15px;border-top: 2px solid black"></p>
-    <v-row>
-      <p style="margin: 20px"> 주문 상품 </p>
+      <v-card v-else class="pt-5 pl-5">
+        <address-form @submit="onAddressSubmit"/>
+      </v-card>
 
-      <p style="margin: 20px; margin-left: 620px; font-size: 13px" >총 {{totalCount}}건</p>
-    </v-row>
+      <order-product-info/>
 
-<!--    바로 구매 (단건)인 경우 : directOrderCheck는 true  -->
-    <!--    데이터 분석 orderList = (orderSave( cartInfoCheck:false, directOrderCheck:true , tmpCartItemOrderNo:0,  quantity, totalPrice, product(productNo, brand, name, price, productInfo(thumnailFileName) ) )-->
-    <v-list v-if="this.orderList.orderSave.directOrderCheck">
-      <v-list-item>
-        <v-card style="width: 760px">
-          <v-list-item-content >
-            <v-row justify="left" v-for="item in orderList" :key="item.tmpCartItemOrderNo">
-              <v-img :src="require(`@/assets/products/defaultImg/${item.product.productInfo.thumbnailFileName}`)"
-                     max-width="60"
-                     style="margin: 15px; margin-left: 25px;"/>
-              <p style="margin-left: 50px; margin-top: 33px">{{ item.product.name}}</p>
-              <p style="margin-left: 400px;  margin-top: 33px; font-weight: bold">{{item.product.price}}원</p>
-              <p style="margin-top: 33px;">/ {{item.quantity}}개</p>
-            </v-row>
-          </v-list-item-content>
-        </v-card>
-      </v-list-item>
-    </v-list>
+      <order-agreement class="mb-5" TotalPaymentAmount="20000"/>
 
-<!--    선택구매, 전체선택 (어러 건)인 경우 : directOrderCheck는 false -->
-    <!--데이터 분석 orderList (orderSave (cartOrderCheck:true, directOrderCheck:false, totalPrice,
-                                        selectList({cartItemNo, count, product(brand, name, price, productInfo(thumnailFileName)) }, {1번째}, {2번째},...)
-                                           ) )-->
-    <v-list v-else>
-      <v-list-item>
-        <v-card style="width: 760px">
-          <v-list-item-content >
-            <v-row justify="left" v-for="item in orderList.orderSave.selectList" :key="item.index">
-              <v-img :src="require(`@/assets/products/defaultImg/${item.product.productInfo.thumbnailFileName}`)"
-                     max-width="60"
-                     style="margin: 15px; margin-left: 25px;"/>
-              <p style="margin-left: 50px; margin-top: 33px">{{ item.product.name}}</p>
-              <p style="margin-left: 400px;  margin-top: 33px; font-weight: bold">{{item.product.price}}원</p>
-              <p style="margin-top: 33px;">/ {{item.count}}개</p>
-            </v-row>
-          </v-list-item-content>
-        </v-card>
-      </v-list-item>
+    </v-container>
 
-    </v-list>
-
-<!--    총 합계 추가 -->
-    <div class="row" style="margin-top: 60px; margin-left: 30px; font-size: 25px; font-weight: bold;">
-      <p class="col-sm-4" style="text-align: right; color: #205c37">총 합계</p>
-      <div class="col-sm-8" align="center" style=" color: #205c37">
-        <p>{{ this.totalPrice | numberFormat }} 원</p>
-      </div>
-    </div>
-
-    <p style="padding-left: 270px"><ButtonGreen
-        @click="payBtn"
-        btn-name="결제하기"
-        width="265px"
-        x-large
-    /></p>
-    </div>
-
+    <p align="center">
+      <ButtonGreen @click="payment" btn-name="결제하기" width="265px" x-large/>
+    </p>
+  </div>
 </template>
 
 <script>
+import OrderAgreement from "@/components/order/OrderAgreement";
+const IMP = window.IMP;
+IMP.init("imp20030584");
 
-import {mapState} from "vuex";
+import {mapActions, mapState} from "vuex";
 
+import AddressForm from "@/components/common/AddressForm";
+import OrderProductInfo from "@/components/order/OrderProductInfo";
 export default {
   name: "OrderInfoForm",
+  components: {OrderProductInfo, OrderAgreement, AddressForm},
+  computed : {
+    ...mapState([
+      'isAuthenticated',
+      'resMember',
+      'resMemberProfile'
+    ]),
+    ...mapState([
+      'orderList'
+    ])
+  },
   data() {
     return{
-      Request:'',
-
-      MemberInfoShow : true,
-      ShowDirection : true,
-      PortalSites :['naver.com', 'gmail.com',],
-      LocalNumber :['010','011','016','017','018','019'],
-      RequestItems:['조심히 안전하게 와주세요~','배송전 미리 연락주세요!','경비실에 맡겨주세요!','조심!또 조심!'],
+      directlyInput:false,
+      request:['문앞에 두고 가주세요' , '조심히 안전하게 와주세요', '출발전 문자 부탁드립니다','부재시 경비실에 맡겨주세요', '직접입력'],
+      selectedRequest:'',
+      directRequest:'',
+      sendRequest:'',
 
       city: '',
       street: '',
@@ -193,75 +110,93 @@ export default {
       totalCount: 0,
 
       //결제 후 장바구니 아이템 삭제용
-      orderCartItemNo:[]
+      orderCartItemNo:[],
+
+      setDestination: true,
+      setAddress: false,
+
+      PaymentPrice: 100,
+      uid_num: 0o00321,
+      merchant_uid :'ORD20221207-',
+      randomNumber : 0,
+      usedNum :[],
+
     }
   },
-  computed:{
-    ...mapState([
-        'orderList'
-    ])
-  },
- created() {
-
-    this.totalPrice =this.$store.state.orderList.orderSave.totalPrice
-   if(this.orderList.orderSave.directOrderCheck){ //바로 구매일 경우
-     this.totalCount = 1
-   }else {  //바로 구매아니고 선택 구매, 전체구매일 경우
-     this.totalCount = Object.keys(this.$store.state.orderList.orderSave.selectList).length
-   }
-
+  mounted() {
+    if(this.$store.state.isAuthenticated === true) {
+      let token = window.localStorage.getItem('userInfo')
+      this.reqMemberInfoToSpring(token)
+      this.reqMemberProfileInfoToSpring(token)
+    } else {
+      alert("로그인 상태가 아닙니다.")
+    }
   },
   methods : {
-    orderMemberInfoVisible(){
-      this.MemberInfoShow = !this.MemberInfoShow;
+    ...mapActions([
+      'reqMemberInfoToSpring',
+      'reqMemberProfileInfoToSpring'
+    ]),
+    setDestinationToAddress(){
+      if(this.setAddress == false ){
+        this.city = this.$store.state.resMemberProfile.address.city
+        this.street = this.$store.state.resMemberProfile.address.street
+        this.addressDetail = this.$store.state.resMemberProfile.address.addressDetail
+        this.zipcode = this.$store.state.resMemberProfile.address.zipcode
 
-      if(this.MemberInfoShow == true){
-        this.ShowDirection = true;
-      }else {
-        this.ShowDirection = false;
+        alert("기본 주소를 배송지로 설정하였습니다.")
+        console.log(this.city + this.street + this.addressDetail +this.zipcode)
       }
+      this.setAddress = !this.setAddress;
     },
-    payBtn(){
-    //if문 해서 this.orderList.orderSave.cartOrderCheck -> true면 cart정보가 더 있게 구성 -> 결제 후 장바구니 지우기 기능에 사용
-    //   for (let i = 0; i <this.orderList.orderSave.selectList.length ; i++) {
-    //     this.orderCartItemNo[i] = this.orderList.orderSave.selectList[i].cartItemNo
-    //   }
-    //this.orderCartItemNo 배열 값 스프링에 전달해서 삭제
+    onAddressSubmit (payload) {
+      const { city, street, addressDetail, zipcode } = payload;
+      this.city = city;
+      this.street = street;
+      this.addressDetail = addressDetail;
+      this.zipcode = zipcode;
 
+      console.log(this.city + this.street + this.addressDetail +this.zipcode)
     },
-    callDaumAddressApi () {
-      new window.daum.Postcode({
-        oncomplete: (data) => {
-          let fullRoadAddr = data.roadAddress;
-          let extraRoadAddr = '';
-          if (data.bname !== '' && /[동|로|가]$/g.test(data.bname)) {
-            extraRoadAddr += data.bname;
-          }
-          if (data.buildingName !== '' && data.apartment === 'Y') {
-            extraRoadAddr += (extraRoadAddr !== '' ? ', ' + data.buildingName : data.buildingName);
-          }
-          if (extraRoadAddr !== '') {
-            extraRoadAddr = ' (' + extraRoadAddr + ')';
-          }
-          if (fullRoadAddr !== '') {
-            fullRoadAddr += extraRoadAddr;
-          }
-          this.city = data.sido;
-          this.zipcode = data.zonecode;
-          this.street = data.sigungu + ' ' + fullRoadAddr;
-          this.streetPass = true
-        }
-      }).open()
-    },
+    payment() {
+
+    }
   },
-  filters: {
-    numberFormat(val) {
-      return String(val).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  beforeUpdate() {
+    if( this.selectedRequest == this.request[4]){
+      this.directlyInput =true;
+      this.sendRequest= this.directRequest;
+    }else {
+      this.directlyInput =false;
+      this.sendRequest = this.selectedRequest;
     }
   },
 }
 </script>
 
 <style scoped>
-
+.member-info-card .member-row {
+  margin-top: 10px;
+  margin-left: 20px;
+}
+.member-info-card .member-request-direct {
+  margin-right: 20px;
+  margin-left: 190px;
+}
+.member-info-card .member-row .member-tool{
+  font-size: 12px;
+  font-weight: bold;
+  width: 170px;
+  margin-right: 0px;
+  padding-right: 0px;
+}
+.member-info-card .member-row .member-info{
+  font-size: 12px;
+}
+.member-info-card .member-row .member-request-select{
+  padding: 0px !important;
+  max-width: 500px;
+  font-size: 12px;
+  min-height: 20px !important;
+}
 </style>
