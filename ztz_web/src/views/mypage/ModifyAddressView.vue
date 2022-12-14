3d511fad4e6a74211ecc7791c5f135d6ecb5cf91
choi jaemin <foertonw5@naver.com>
2022-12-14 18:04:45 +0900

    feat:[최재민] - 배송지 변경하기 버튼을 클릭시 새로운 배송지로 수정할 수있는 페이지 UI[ZTZ-WEB-UI51]

---
+++ ztz_web/src/views/mypage/ModifyAddressView.vue
@@ -0,0 +1,74 @@
+<template>
+  <div align="center">
+    <v-img
+        :src="require('@/assets/ztz_logo1.png')" width="120" />
+    <v-card class="pa-5 mb-5" outlined>
+      <address-form @submit="onAddressSubmit"/>
+    </v-card>
+    <button-green @click="modifyAddress" btn-name="배송지 저장" />
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import {mapState} from "vuex";
+
+export default {
+  name: "ModifyAddressView",
+  computed : {
+    ...mapState([
+      'isAuthenticated',
+    ])
+  },
+  mounted() {
+    if(this.$store.state.isAuthenticated === true) {
+      this.token = window.localStorage.getItem('userInfo')
+    } else {
+      alert("로그인 상태가 아닙니다.")
+    }
+  },
+  data(){
+    return{
+
+      city: '',
+      street: '',
+      addressDetail: '',
+      zipcode: '',
+
+      token :''
+    }
+  },
+  methods:{
+    onAddressSubmit (payload) {
+      const { city, street, addressDetail, zipcode } = payload;
+      this.city = city;
+      this.street = street;
+      this.addressDetail = addressDetail;
+      this.zipcode = zipcode;
+
+      console.log(this.city + this.street + this.addressDetail +this.zipcode)
+    },
+    async modifyAddress(){
+      const { city, street, addressDetail, zipcode, token } = this
+
+       await axios.post("http://localhost:7777/ztz/member/modify-address", {
+        city, street, addressDetail, zipcode , token
+      })
+          .then((res) => {
+            alert("배송지 설정이 완료 되었습니다.")
+            console.log(res)
+          })
+          .catch((res) => {
+            alert(res.response.data.message)
+          })
+
+      opener.location.reload();
+      window.close()
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
