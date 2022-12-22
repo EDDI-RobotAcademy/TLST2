ee3eec98b284ac6ef35539fa7fab76777a465391
tonowoods <tono.woods@gmail.com>
2022-12-22 16:53:52 +0900

    feat: [임보경] 사용자가 예약 완료 후 마이페이지를 누르면 보이는 예약 내역[ZTZ-WEB-UI63]

--- ztz_web/src/components/mypage/MyPageProfileSection.vue
+++ ztz_web/src/components/mypage/MyPageProfileSection.vue
@@ -1,6 +1,5 @@
 <template>
   <section class="profile-section">
-
     <div class="management-box first-box">
       <p class="name head-text">{{ this.$store.state.resMember.username }}</p>
       <p class="email">{{ this.$store.state.resMember.email }}</p>
@@ -8,66 +7,87 @@
 
     <nav>
       <ul class="sub-nav">
-        <li><a href="#">회원정보변경</a></li>
-        <li><a href="#">배송지관리</a></li>
-        <li><a href="#">주문관리</a></li>
-        <li>
-          <router-link :to="{ name: 'MyPageReview', params: {memberId: resMember.id.toString()} }">
-          상품 리뷰
-        </router-link>
+        <li
+          v-for="(single, i) in mypageNavi"
+          :key="i"
+          @click="goToEachComponent(single)"
+        >
+          {{ single.menu }}
         </li>
-        <li><a href="#">예약관리</a></li>
+
+        <li>
+          <router-link
+            :to="{
+              name: 'MyPageReview',
+              params: { memberId: resMember.id.toString() },
+            }"
+          >
+            상품 리뷰
+          </router-link>
+        </li>
         <li><a @click="withdrawal">회원탈퇴</a></li>
       </ul>
     </nav>
-
   </section>
 </template>
 
 <script>
-import {mapActions, mapState} from "vuex";
+import { mapActions, mapState } from "vuex";
 export default {
   name: "MyPageProfileSection",
-  computed : {
-    ...mapState([
-      'isAuthenticated',
-      'resMember',
-    ])
+  data() {
+    return {
+      mypageNavi: [
+        { menu: "회원정보변경", componentsName: "", index: 0 },
+        { menu: "배송지관리", componentsName: "", index: 1 },
+        { menu: "주문관리", componentsName: "", index: 2 },
+        { menu: "상품 리뷰", componentsName: "", index: 3 },
+        { menu: "예약관리", componentsName: "MyReservationDetail", index: 4 },
+        { menu: "회원탈퇴", componentsName: "", index: 5 },
+      ],
+      propData: {},
+    };
   },
-  mounted(){
-    if(this.$store.state.isAuthenticated === true) {
-      let token = window.localStorage.getItem('userInfo')
-      this.reqMemberInfoToSpring(token)
+  computed: {
+    ...mapState(["isAuthenticated", "resMember"]),
+  },
+  mounted() {
+    if (this.$store.state.isAuthenticated === true) {
+      let token = window.localStorage.getItem("userInfo");
+      this.reqMemberInfoToSpring(token);
     } else {
-      console.log("내 상태가 로그인상태가 아니라면 자동으로 로그인창으로 넘어갈수 있나?")
-      alert("로그인 상태가 아닙니다.")
+      console.log(
+        "내 상태가 로그인상태가 아니라면 자동으로 로그인창으로 넘어갈수 있나?"
+      );
+      alert("로그인 상태가 아닙니다.");
     }
-
   },
-  methods : {
-    ...mapActions([
-        'reqDeleteMemberToSpring',
-        'reqMemberInfoToSpring'
-    ]),
+  methods: {
+    ...mapActions(["reqDeleteMemberToSpring", "reqMemberInfoToSpring"]),
+    goToEachComponent(single) {
+      alert(single.componentsName);
+      this.$router.push({
+        name: "MypageDetailView",
+        params: { name: single.componentsName, index: single.index },
+      });
+    },
     async withdrawal() {
-      let withdrawalMessage = confirm("회원 탈퇴하시겠습니까?")
+      let withdrawalMessage = confirm("회원 탈퇴하시겠습니까?");
 
-      if(withdrawalMessage){
-        let token = window.localStorage.getItem('userInfo')
+      if (withdrawalMessage) {
+        let token = window.localStorage.getItem("userInfo");
         // eslint-disable-next-line no-undef
-        await this.reqDeleteMemberToSpring(token)
-        console.log(this.$store.state.resMyRequest.toString())
-        this.$store.commit('IS_AUTHENTICATED', false)
-        localStorage.removeItem("userInfo")
-        this.$cookies.remove('user')
-        alert('회원탈퇴가 완료되었습니다. 그동안 이용해주셔서 감사드립니다.')
-        await this.$router.push({name:'HomeView'})
+        await this.reqDeleteMemberToSpring(token);
+        console.log(this.$store.state.resMyRequest.toString());
+        this.$store.commit("IS_AUTHENTICATED", false);
+        localStorage.removeItem("userInfo");
+        this.$cookies.remove("user");
+        alert("회원탈퇴가 완료되었습니다. 그동안 이용해주셔서 감사드립니다.");
+        await this.$router.push({ name: "HomeView" });
       }
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- ztz_web/src/router/index.js
+++ ztz_web/src/router/index.js
@@ -1,49 +1,62 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+import HomeView from "@/views/HomeView";
+
 import SignUpView from "@/views/account/SignUpView";
 import SignInView from "@/views/account/SignInView";
-import ProductDetailView from "@/views/products/ProductDetailView";
-import CartView from "@/views/order/CartView";
+
 import ProductsView from "@/views/products/ProductsView";
+import ProductDetailView from "@/views/products/ProductDetailView";
+import ProductRegisterView from "@/views/products/ProductRegisterView";
+import ProductModifyView from "@/views/products/ProductModifyView";
+
+import CartView from "@/views/order/CartView";
 import OrderInfoView from "@/views/order/OrderInfoView";
+
 import MypageView from "@/views/mypage/MypageView";
-import HomeView from "@/views/HomeView";
+import MyPageReview from "@/views/mypage/MyPageReview";
+import MypageDetailView from "@/views/mypage/MypageDetailView";
+import ModifyAddressView from "@/views/mypage/ModifyAddressView";
+
+import QuestionListView from "@/views/boards/QuestionListView";
 import QuestionReadView from "@/views/boards/QuestionReadView";
 import QuestionRegisterView from "@/views/boards/QuestionRegisterView";
 import QuestionModifyView from "@/views/boards/QuestionModifyView";
-import MyPageReview from "@/views/mypage/MyPageReview";
 
 import FoundryView from "@/views/tour/FoundryView.vue";
 import ReservationView from "@/views/tour/ReservationView.vue";
-import ModifyAddressView from "@/views/mypage/ModifyAddressView";
-import ProductRegisterView from "@/views/products/ProductRegisterView";
-import ProductModifyView from "@/views/products/ProductModifyView";
 import CompleteView from "@/views/tour/CompleteView";
-import QuestionListView from "@/views/boards/QuestionListView";
+
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: "/",
+    name: "home",
+    component: HomeView,
   },
   {
-    path: '/sign-up',
-    name: 'SignUpView',
-    component: SignUpView
+    path: "/sign-up",
+    name: "SignUpView",
+    component: SignUpView,
   },
   {
-    path: '/sign-in',
-    name: 'SignInView',
-    component: SignInView
+    path: "/sign-in",
+    name: "SignInView",
+    component: SignInView,
   },
   {
-    path: '/product',
-    name: 'ProductsView',
-    component: ProductsView
+    path: "/product",
+    name: "ProductsView",
+    component: ProductsView,
+  },
+  {
+    path: "/product/:productNo",
+    name: "ProductDetailView",
+    component: ProductDetailView,
+    props: true,
   },
   {
     path: '/product-register',
@@ -51,57 +64,52 @@ const routes = [
     component: ProductRegisterView
   },
   {
-    path: '/product/:productNo',
-    name: 'ProductDetailView',
-    component: ProductDetailView,
-    props: true
-  },
-
-  {
     path: '/product-modify/:productNo',
     name: 'ProductModifyView',
     component: ProductModifyView,
     props: true
   },
   {
-    path: '/cart',
-    name: 'CartView',
-    component: CartView
+    path: "/cart",
+    name: "CartView",
+    component: CartView,
   },
   {
-    path: '/order-Info',
-    name: 'OrderInfoView',
-    component: OrderInfoView
+    path: "/order-Info",
+    name: "OrderInfoView",
+    component: OrderInfoView,
   },
   {
     path: '/my-page',
     name: 'MyPageView',
     component: MypageView
   },
-
   {
-    path: '/tour',
-    name: 'FoundryView',
-    component: FoundryView
+    path: "/my-page-detail",
+    name: "MypageDetailView",
+    component: MypageDetailView,
+    // true로 설정하면 데이터를 props로도 받습니다.
+    props: true
   },
   {
-    path: '/reservation',
-    name: 'ReservationView',
-    component: ReservationView
-  },
-  {
-    path: "/completeReservation",
-    name: "CompleteView",
-    component: CompleteView,
+    path: '/my-page/review/:memberId',
+    name: 'MyPageReview',
+    components: {
+      default: MyPageReview
+    },
+    props: {
+      default: true
+    }
   },
   {
     path: '/modify-address',
     name: 'ModifyAddressView',
     component: ModifyAddressView,
     meta:{
-      hideHeader:true,
+        hideHeader:true,
     }
   },
+
   {
     path: "/question-list",
     name: "QuestionListView",
@@ -111,10 +119,10 @@ const routes = [
     path: "/question-read/:questionNo",
     name: "QuestionReadView",
     components: {
-      default: QuestionReadView,
+        default: QuestionReadView,
     },
     props: {
-      default: true,
+        default: true,
     },
   },
   {
@@ -126,28 +134,35 @@ const routes = [
     path: "/question-modify/:questionNo",
     name: "QuestionModifyView",
     components: {
-      default: QuestionModifyView,
+        default: QuestionModifyView,
     },
     props: {
-      default: true,
+        default: true,
     },
   },
   {
-    path: '/my-page/review/:memberId',
-    name: 'MyPageReview',
-    components: {
-      default: MyPageReview
-    },
-    props: {
-      default: true
-    }
-  }
+    path: '/tour',
+    name: 'FoundryView',
+    component: FoundryView
+  },
+  {
+    path: "/reservation",
+    name: "ReservationView",
+    component: ReservationView,
+    props: true,
+  },
+  {
+    path: "/completeReservation",
+    name: "CompleteView",
+    component: CompleteView,
+  },
+
 ]
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
+  routes,
 })
 
 export default router
