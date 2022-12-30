a50c4fdc69e70160a735f9f9cfcb1d3aed67d09d
choi jaemin <foertonw5@naver.com>
2022-12-30 16:31:39 +0900

    style :[최재민] 앱 내에서 사용하는 colorStyle[ZTZ-APP-UI4]

--- ztz_app/lib/pages/main_page/main_page.dart
+++ ztz_app/lib/pages/main_page/main_page.dart
@@ -2,15 +2,25 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import '../../components/main_page_components/main_component.dart';
+import '../../utility/text_styles.dart';
 
 class MainPage extends StatelessWidget{
   const MainPage ({Key?key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SafeArea(
-        child: MainComponent(),
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              margin: EdgeInsets.only(top:12 , bottom: 5,left: 3),
+              child: Text("👍 오늘의 추천 ZTZ",style: productTitleTextStyle(),textAlign: TextAlign.left,),
+            ),
+            MainComponent(),
+          ],
+        ),
       ),
     );
   }
--- ztz_app/lib/utility/colors.dart
+++ ztz_app/lib/utility/colors.dart
@@ -4,4 +4,5 @@ class ColorStyle {
   static const mainColor = Color(0xff205C37);
   static const textGray = Color(0xffCBCBCC);
   static const textPink = Color(0xffEB4646);
+  static const termGray  = Color(0xffE4E4E4);
 }
