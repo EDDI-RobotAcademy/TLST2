c59958e6d8e3001ca37dffd7c5da054955bea3ae
dobbymun <tearofcake@gmail.com>
2022-12-31 22:58:42 +0900

    fix:[박진주]서버로 부터 모든 제품 정보를 받아서 화면에 출력하는 컴포넌트UI[ZTZ-APP-UI11]

--- ztz_app/lib/components/product/product_detail/prdouct_buy_modal_component.dart
+++ ztz_app/lib/components/product/product_detail/prdouct_buy_modal_component.dart
@@ -32,116 +32,118 @@ class _ProductBuyModalComponent extends State<ProductBuyModalComponent>{
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Container(
-      child: Column(
-        children: [
-          SizedBox(
-            height: 20
-          ),
-          SizedBox(
-            width: 40,
-            child: Divider(height: 3, thickness: 3,color: Colors.grey),
-          ),
-          Container(
-            margin: EdgeInsets.only(top: 20, left: 20),
-            child: Row(
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                      border: Border.all(color: Colors.black,width: 0.1)
-                  ),
-                  width: 70,
-                  child: Image(image: AssetImage("assets/images/Hongju.jpg"),),
-                ),
-                Container(
-                  padding: EdgeInsets.only(left: 15),
-                  child: Column(
-                    children: [
-                      Container(
-                        padding: EdgeInsets.only(top: 10,bottom: 5),
-                        width: 100,
-                        child: Text(productName,textAlign: TextAlign.left,style: defaultTextStyle(),),
-                      ),
-                      Text(subTitle,style: mediumGrayTextStyle(),),
-                      Container(
-                          padding: EdgeInsets.only(top: 10,bottom: 10),
-                          width: 100,
-                          child: Text(ProductInfo.taste.toString(),textAlign: TextAlign.left,style: xSmallPinkTextStyle(),)),
-                    ],
-                  ),
-                )
-              ],
+    return SingleChildScrollView(
+      child: Container(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 20
             ),
-          ),
-          Divider(thickness: 1),
-          Container(
-            padding: EdgeInsets.only(left: 15),
-            width: size.width,
-            child: Column(
-              children: [
-                Container(
-                    padding: EdgeInsets.only(top: 10,bottom: 10),
-                    width:size.width ,
-                    child: Row(
-                      children: [
-                        Text("🚛 배송비",textAlign: TextAlign.left,style: MediumBlackTextStyle()),
-                        SizedBox(width: size.width-(size.width/1.7),),
-                        Text("ztz는 모든 배송비가 무료!",textAlign: TextAlign.right,style: MediumBlackTextStyle())
-                      ],
-                    )
-                ),
-                Container(
-                    padding: EdgeInsets.only(top: 10,bottom: 10),
-                    width:size.width ,
-                    child: Row(
-                      children: [
-                        Text("🎁 상품가격",textAlign: TextAlign.left,style: MediumBlackTextStyle()),
-                        SizedBox(width: size.width-(size.width/2.4),),
-                        Text(numberFormat.format(productPrice)+"원",textAlign: TextAlign.right,style: MediumBlackTextStyle())
-                      ],
-                    )
-                ),
-                Container(
-                    padding: EdgeInsets.only(top: 10,bottom: 10),
-                    width:size.width ,
-                    child: Row(
-                      children: [
-                        Container(
-                            padding: EdgeInsets.only(right: size.width-(size.width/2.3)),
-                            child: Text("✔수량",textAlign: TextAlign.left,style: MediumBlackTextStyle(),)
-                        ),
-                        amountOfProduct()
-                      ],
-                    )),
-              ],
+            SizedBox(
+              width: 40,
+              child: Divider(height: 3, thickness: 3,color: Colors.grey),
             ),
-          ),
-          Divider(thickness: 1),
-          Container(
-              padding: EdgeInsets.only(left: 15,top: 5,bottom: 5),
-              width: size.width,
+            Container(
+              margin: EdgeInsets.only(top: 20, left: 20),
               child: Row(
                 children: [
-                  Text("총 결제금액" ,style: lageBlackTextStyle(),),
                   Container(
-                      padding: EdgeInsets.only(left: size.width-(size.width/1.8)),
-                      child: Text(numberFormat.format(productPrice * selectedAmount) +"원",style: lageBlackTextStyle()
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black,width: 0.1)
+                    ),
+                    width: 70,
+                    child: Image(image: AssetImage("assets/images/uploadImg/${image}"),),
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(left: 15),
+                    child: Column(
+                      children: [
+                        Container(
+                          padding: EdgeInsets.only(top: 10,bottom: 5),
+                          width: 100,
+                          child: Text(productName,textAlign: TextAlign.left,style: defaultTextStyle(),),
+                        ),
+                        Text(subTitle,style: mediumGrayTextStyle(),),
+                        Container(
+                            padding: EdgeInsets.only(top: 10,bottom: 10),
+                            width: 100,
+                            child: Text(ProductInfo.taste.toString(),textAlign: TextAlign.left,style: xSmallPinkTextStyle(),)),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Divider(thickness: 1),
+            Container(
+              padding: EdgeInsets.only(left: 15),
+              width: size.width,
+              child: Column(
+                children: [
+                  Container(
+                      padding: EdgeInsets.only(top: 10,bottom: 10),
+                      width:size.width ,
+                      child: Row(
+                        children: [
+                          Text("🚛 배송비",textAlign: TextAlign.left,style: MediumBlackTextStyle()),
+                          SizedBox(width: size.width-(size.width/1.7),),
+                          Text("ztz는 모든 배송비가 무료!",textAlign: TextAlign.right,style: MediumBlackTextStyle())
+                        ],
                       )
                   ),
+                  Container(
+                      padding: EdgeInsets.only(top: 10,bottom: 10),
+                      width:size.width ,
+                      child: Row(
+                        children: [
+                          Text("🎁 상품가격",textAlign: TextAlign.left,style: MediumBlackTextStyle()),
+                          SizedBox(width: size.width-(size.width/2.4),),
+                          Text(numberFormat.format(productPrice)+"원",textAlign: TextAlign.right,style: MediumBlackTextStyle())
+                        ],
+                      )
+                  ),
+                  Container(
+                      padding: EdgeInsets.only(top: 10,bottom: 10),
+                      width:size.width ,
+                      child: Row(
+                        children: [
+                          Container(
+                              padding: EdgeInsets.only(right: size.width-(size.width/2.3)),
+                              child: Text("✔수량",textAlign: TextAlign.left,style: MediumBlackTextStyle(),)
+                          ),
+                          amountOfProduct()
+                        ],
+                      )),
                 ],
-              )
-          ),
-          Container(
-            padding: EdgeInsets.all(5),
-            child: Row(
-              children: [
-                ElevatedButton(onPressed: (){}, child: Text("장바구니",style: xMediumWhiteTextStyle(),),style: defaultElevatedButtonStyle((size.width/2)-10,50),),
-                SizedBox(width: 10,),
-                ElevatedButton(onPressed: (){}, child: Text("결제하기",style: xMediumWhiteTextStyle()),style: defaultElevatedButtonStyle((size.width/2)-10,50)),
-              ],
+              ),
             ),
-          )
-        ],
+            Divider(thickness: 1),
+            Container(
+                padding: EdgeInsets.only(left: 15,top: 5,bottom: 5),
+                width: size.width,
+                child: Row(
+                  children: [
+                    Text("총 결제금액" ,style: lageBlackTextStyle(),),
+                    Container(
+                        padding: EdgeInsets.only(left: size.width-(size.width/1.8)),
+                        child: Text(numberFormat.format(productPrice * selectedAmount) +"원",style: lageBlackTextStyle()
+                        )
+                    ),
+                  ],
+                )
+            ),
+            Container(
+              padding: EdgeInsets.all(5),
+              child: Row(
+                children: [
+                  ElevatedButton(onPressed: (){}, child: Text("장바구니",style: xMediumWhiteTextStyle(),),style: defaultElevatedButtonStyle((size.width/2)-10,50),),
+                  SizedBox(width: 10,),
+                  ElevatedButton(onPressed: (){}, child: Text("결제하기",style: xMediumWhiteTextStyle()),style: defaultElevatedButtonStyle((size.width/2)-10,50)),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
