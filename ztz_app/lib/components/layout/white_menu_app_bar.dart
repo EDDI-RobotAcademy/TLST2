fd4c93c3de5da5f9a901fc267f15b767dd8ba588
ramram222 <kim.ram454@gmail.com>
2023-01-06 01:15:44 +0900

    update: [김가람] 앱 내에서 사용하는 각 메뉴별 하얀색 상단 바 추가 [ZTZ-APP-UI16]

---
+++ ztz_app/lib/components/layout/white_menu_app_bar.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:ztz_app/utility/text_styles.dart';
+
+class WhiteMenuAppBar extends StatefulWidget with PreferredSizeWidget {
+  const WhiteMenuAppBar({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<WhiteMenuAppBar> createState() => _WhiteMenuAppBarState();
+
+  @override
+  Size get preferredSize => Size.fromHeight(50);
+}
+
+// 하얀색 앱 바 + 검은색 글자 + 장바구니 버튼 없음
+class _WhiteMenuAppBarState extends State<WhiteMenuAppBar> {
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      iconTheme: IconThemeData(
+        color: Colors.grey,
+      ),
+      elevation: 0.1,
+      // title => 카테고리, 검색, 마이페이지 등
+      title: Text(widget.title, style: blackBoldTextStyle(16)),
+      centerTitle: true,
+      backgroundColor: Colors.white,
+    );
+  }
+}
