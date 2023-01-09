1d12beb1ed7745141120caf1675f685db94ab49c
choi jaemin <foertonw5@naver.com>
2023-01-09 20:29:56 +0900

    feat : [최재민] 카테고리 에서 해당 카테고리 클릭 시 나오는 상품 리스트UI[ZTZ-APP-UI34]

---
+++ ztz_app/lib/components/product/product_list/product_list_component.dart
@@ -0,0 +1,230 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+
+import '../../../controller/product/product_controller.dart';
+import '../../../controller/product/product_infos/product_info.dart';
+import '../../../controller/reivew/review_controller.dart';
+import '../../../pages/product/product_detail_page.dart';
+import '../../../utility/colors.dart';
+import '../../../utility/text_styles.dart';
+import '../../main_page_components/product_card.dart';
+
+class ProductListComponent extends StatefulWidget {
+  const ProductListComponent({Key? key, required this.drinkItem}) : super(key: key);
+
+  final String drinkItem;
+
+  @override
+  State<ProductListComponent> createState() => _ProductListComponent();
+}
+
+class _ProductListComponent extends State<ProductListComponent> {
+
+  bool isLoading = false;
+  List<String> _localList = ['전체지역','서울경기','강원','충청','경상','전라','제주'];
+  var _selectedLocal = '전체지역';
+  ScrollController _mainScrollController = ScrollController();
+  ScrollController _subScrollController = ScrollController();
+
+  double _removableWidgetSize = 150;
+  bool _isStickyOnTop = false;
+
+  void setProduct() async {
+    switch (widget.drinkItem) {
+      case "전체보기":
+        await ProductController().requestAllProductToSpring("");
+        break;
+      case "소주증류주":
+        await ProductController().requestProductByAlcoholType(widget.drinkItem);
+        break;
+      case "리큐르":
+        await ProductController().requestProductByAlcoholType(widget.drinkItem);
+        break;
+      case "막걸리":
+        await ProductController().requestProductByAlcoholType(widget.drinkItem);
+        break;
+      case "약주청주":
+        await ProductController().requestProductByAlcoholType(widget.drinkItem);
+        break;
+      case "과실주":
+        await ProductController().requestProductByAlcoholType(widget.drinkItem);
+        break;
+      case "기타주류":
+        await ProductController().requestProductByAlcoholType(widget.drinkItem);
+        break;
+    }
+    setState(() {
+      isLoading = true;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _mainScrollController.addListener(() {
+      if (_mainScrollController.offset >= _removableWidgetSize &&
+          !_isStickyOnTop) {
+        _isStickyOnTop = true;
+        setState(() {});
+      } else if (_mainScrollController.offset < _removableWidgetSize &&
+          _isStickyOnTop) {
+        _isStickyOnTop = false;
+        setState(() {});
+      }
+    });
+    setProduct();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (!isLoading) { // 로딩 판단 유무
+      return const Padding( // 로딩시 나오는 동그라미 동글동글
+        padding: EdgeInsets.all(100),
+        child: Center(
+          child: CircularProgressIndicator(
+            color: ColorStyle.mainColor,
+          ),
+        ),
+      );
+    } else {
+      return Container( // 로딩 되면 나오는 위젯
+        child: gridList(),
+      );
+    }
+  }
+
+  Widget gridList() {
+    return Column(
+      children: [
+        Flexible(
+          child: Stack(
+            children: [
+              ListView(
+                controller: _mainScrollController,
+                shrinkWrap: true,
+                children: [
+                  Container(
+                      height: _removableWidgetSize,
+                      child: Image(image: AssetImage("assets/images/bigSale.jpg"),fit: BoxFit.fill,)),
+                  _getStickyWidget(),
+                  GridView.count(
+                    controller: _subScrollController,
+                    padding: EdgeInsets.only(top: 4, right: 20),
+                    shrinkWrap: true,
+                    crossAxisCount: 2,
+                    scrollDirection: Axis.vertical,
+                    childAspectRatio: MediaQuery.of(context).size.height / 1500,
+                    children: List.generate(ProductInfo.productList.length, (index) =>
+                        Padding(
+                          padding: const EdgeInsets.only(left: 10),
+                          child: ProductCard(
+                            brand: ProductInfo.productList[index]['brand'],
+                            price: ProductInfo.productList[index]['price'],
+                            onTap: () {
+                              selectProductCard(index);
+                            },
+                            title: ProductInfo.productList[index]['name'],
+                            image: ProductInfo.productList[index]['productInfo']['thumbnailFileName'],
+                          ),
+                        )
+                    ),
+                  )
+                ],
+              ),
+              if (_isStickyOnTop) _getStickyWidget()
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Container _getStickyWidget() {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      color: Colors.white,
+      padding: EdgeInsets.only(left: 30 , top: 5, bottom: 5),
+      child: Row(
+        children: [
+          Text("총" + ProductInfo.productList.length.toString() +"개",style: MediumBlackTextStyle(),),
+          SizedBox(
+            width: size.width - (size.width / 2.5),
+          ),
+          DropdownButton(
+            style: MediumBlackTextStyle(),
+            underline : Container(),
+            value: _selectedLocal,
+            items: _localList.map(
+                    (String value) {
+                      return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value)
+                      );
+                    },
+            ).toList(),
+            onChanged: (String? value) {
+              setState(() {
+                isLoading = false;
+                _selectedLocal = value!;
+                if(widget.drinkItem == "전체보기"){
+                  if(_selectedLocal == "전체지역"){
+                    setProduct();
+                  }else{
+                    requestProductByLocal(_selectedLocal);
+                  }
+                }else{
+                  if(_selectedLocal == "전체지역"){
+                    setProduct();
+                  }else{
+                    requestProductByLocalAndType(widget.drinkItem , _selectedLocal);
+                  }
+                }
+              });
+            },
+          )
+        ],
+      )
+    );
+  }
+
+  void requestProductByLocal(localName) async {
+    await ProductController().requestProductByLocal(localName);
+    setState(() {
+      isLoading = true;
+    });
+  }
+
+  void requestProductByLocalAndType(alcoholType , localName) async {
+    await ProductController().requestProductByLocalAndAlcoholType(alcoholType , localName);
+    setState(() {
+      isLoading = true;
+    });
+  }
+
+
+  void selectProductCard(int index) async {
+    await ProductController().requestProductDetailToSpring(
+        ProductInfo.productList[index]['productNo']);
+    await ReviewController().requestReviewAverageToSpring(
+        ProductInfo.productList[index]['productNo']);
+    await ReviewController().requestProductReviewToSpring(
+        ProductInfo.productList[index]['productNo']);
+    Get.to(
+        ProductDetailPage(
+          productNo: ProductInfo.productList[index]['productNo'],
+        ),
+        //next page class
+        duration: Duration(milliseconds: 500),
+        //duration of transitions, default 1 sec
+        transition: Transition.rightToLeft,
+        //transition effect
+        popGesture: true // 슬라이드로 뒤로가기
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+}
