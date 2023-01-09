699d31c1bddde8d7cdf33e61e81defcd02524159
choi jaemin <foertonw5@naver.com>
2023-01-09 20:37:35 +0900

    feat : [최재민] 주문 정보 리스트를 받아오는 api[ZTZ-APP-UI34]

--- ztz_app/lib/controller/order/order_controller.dart
+++ ztz_app/lib/controller/order/order_controller.dart
@@ -1,11 +1,12 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
 import 'package:ztz_app/controller/order/order_infos/order_info.dart';
 
 class OrderController{
-  static const httpUri = '172.30.1.54:7777';
+  static const httpUri = '192.168.0.8:7777';
 
   requestPaymentListFromSpring(token) async {
     var data = {'token' : token};
@@ -17,12 +18,38 @@ class OrderController{
         body: body,
       );
       if(paymentResponse.statusCode == 200) {
+        var tmpPaymentList = [];
         debugPrint("paymentList 결과: " + utf8.decode(paymentResponse.bodyBytes).toString());
-        OrderInfo.paymentList = jsonDecode(utf8.decode(paymentResponse.bodyBytes));
+        tmpPaymentList = jsonDecode(utf8.decode(paymentResponse.bodyBytes));
+        OrderInfo.paymentList.clear();
+        OrderInfo.paymentList = tmpPaymentList.obs;
       } else {
         debugPrint("payment 통신 오류" + paymentResponse.statusCode.toString());
       }
     } catch(e) {
+      debugPrint("오류 발생" + e.toString());
+    }
+  }
+
+  requestPaymentListByDataFromSpring(token , readData) async {
+    var data = {'token' : token , 'readData' : readData};
+    var body = json.encode(data);
+    try{
+      var paymentResponse = await http.post(
+        Uri.http(httpUri, 'ztz/order/readPayment/byDate'),
+        headers: {"Content-Type": "application/json"},
+        body: body,
+      );
+      if(paymentResponse.statusCode == 200) {
+        var tmpPaymentList = [];
+        debugPrint("paymentList 결과: " + utf8.decode(paymentResponse.bodyBytes).toString());
+        tmpPaymentList = jsonDecode(utf8.decode(paymentResponse.bodyBytes));
+        OrderInfo.paymentList.clear();
+        OrderInfo.paymentList = tmpPaymentList.obs;
+      } else {
+        debugPrint("기간 payment 통신 오류" + paymentResponse.statusCode.toString());
+      }
+    }catch(e){
       debugPrint("오류 발생" + e.toString());
     }
   }
