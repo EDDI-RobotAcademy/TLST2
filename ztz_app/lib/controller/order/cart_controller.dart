4e371f11deaa8aecabf2db30873671d96bd3c0dc
tonowoods <tono.woods@gmail.com>
2023-01-09 16:40:03 +0900

    feat: [임보경] 카트의 담긴 상품의 수량을 증가, 감소 시킬 수 있는 widget 기능[ZTZ-APP-UI41]

--- ztz_app/lib/controller/order/cart_controller.dart
+++ ztz_app/lib/controller/order/cart_controller.dart
@@ -5,7 +5,7 @@ import 'package:ztz_app/controller/order/api/order_service_api.dart';
 class CartController extends GetxController{
   static late var reactiveCartList = [].obs;
   static RxBool obsCartStatus = false.obs;
-
+  static var checkStatusMap = <String, bool>{ }.obs;
 
   @override
   void onInit() {
@@ -34,5 +34,34 @@ class CartController extends GetxController{
   Future<void> fetchData() async{
     await OrderService.fetchItems("6688e783-7d19-4edf-a967-0e09aeb1e56b");
   }
+  reqChangeItemCount(int itemNo, int count, int totalPrice) async {
+    var res = await OrderService.requestChangeItemInfo(itemNo, count, totalPrice);
+    if(res == 1) {
+      fetchData();
+      CartController.checkStatusMap.remove(itemNo.toString());
+    } else {
+      print("change failed");
+    }
+  }
 
+  increment(int index){
+    reactiveCartList[index]['count'] ++;
+    int amount =
+        reactiveCartList[index]['count'] * reactiveCartList[index]['product']['price'];
+    reqChangeItemCount(
+        reactiveCartList[index]['itemNo'],
+        reactiveCartList[index]['count'],
+        amount
+    );
+  }
+  decrement(int index){
+    reactiveCartList[index]['count'] --;
+    int amount =
+        reactiveCartList[index]['count'] * reactiveCartList[index]['product']['price'];
+    reqChangeItemCount(
+        reactiveCartList[index]['itemNo'],
+        reactiveCartList[index]['count'],
+        amount
+    );
+  }
 }
