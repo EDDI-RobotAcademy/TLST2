eb3e6197e039b13e7bebb93ff4bbc782991d367e
dobbymun <tearofcake@gmail.com>
2023-01-10 22:53:00 +0900

    feat:[박진주]메인페이지 서브메뉴 버튼 UI[ZTZ-APP-UI55]

--- ztz_app/lib/components/main_page_components/main_component.dart
+++ ztz_app/lib/components/main_page_components/main_component.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'package:get/get.dart';
 import 'package:ztz_app/components/main_page_components/todays_recommend_component.dart';
+import 'package:ztz_app/pages/product/product_list_page.dart';
+import 'package:ztz_app/utility/colors.dart';
 
-import '../../controller/account/sign_up_infos/account_state.dart';
-import '../../pages/home_page.dart';
+import '../../pages/tour/foundry_page.dart';
 import '../../utility/text_styles.dart';
 import 'main_foundry_list_component.dart';
 
@@ -19,11 +19,20 @@ class MainComponent extends StatefulWidget {
 class _MainComponent extends State<MainComponent>{
   static final signInStorage = new FlutterSecureStorage();
 
+  var subMenuList = [
+  {'title': "전통주", 'image': "assets/images/banner/submenu1.png", 'page': ProductListPage( drinkItemIndex: 0)},
+  {'title': "이달의 술", 'image': "assets/images/banner/submenu2.png", 'page': ProductListPage( drinkItemIndex: 0)},
+  {'title': "양조장", 'image': "assets/images/banner/submenu3.png", 'page': FoundryPage()},
+  {'title': "베스트", 'image': "assets/images/banner/submenu4.png", 'page': ProductListPage( drinkItemIndex: 0)},
+  {'title': "좋아요", 'image': "assets/images/banner/submenu5.png", 'page': ProductListPage( drinkItemIndex: 0)},
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Form(
       child: Column(
         children: [
+          subMenuIcon(),
           Container(
             width: double.infinity,
             margin: EdgeInsets.only(top:20 , bottom: 12,left: 3),
@@ -36,15 +45,44 @@ class _MainComponent extends State<MainComponent>{
             child: Text("🏫 TOUR + EXPERIENCE", style: productTitleTextStyle(),textAlign: TextAlign.left,),
           ),
           MainFoundryListComponent(),
-          ElevatedButton(
-              onPressed: () async{
-                AccountState().isLogin = false.obs;
-                await AccountState.signInStorage.deleteAll();
-               // Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => HomePage() ));
-              },
-              child: const Text("임시 로그아웃")),
         ],
       ),
+    );
+  }
+
+  Widget subMenuIcon(){
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child:
+        Row(
+          children: [
+            for (int i = 0; i < subMenuList.length; i++)
+            Column(
+              children: [
+                Container(
+                    margin: const EdgeInsets.only(left: 5, right:5),
+                    decoration: BoxDecoration(
+                        color: ColorStyle.termGray,
+                        borderRadius: BorderRadius.circular(25),
+                    ),
+                    child: Column(
+                      children: [
+                        IconButton(
+                          icon: Image.asset(subMenuList[i]['image'].toString()),
+                          iconSize: 50,
+                          onPressed: () {
+                            Navigator.push(context, MaterialPageRoute(builder: (context) => subMenuList[i]['page'] as Widget ));
+                          },
+                        ),
+                      ],
+                    )
+                ),
+                SizedBox(height: 7),
+                Text(subMenuList[i]['title'].toString() , style: TextStyle( fontWeight: FontWeight.w700 , fontSize: 15)),
+              ],
+            ),
+          ],
+        ),
     );
   }
 }
