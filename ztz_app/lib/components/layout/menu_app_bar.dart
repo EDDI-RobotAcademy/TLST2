4cd05131d91a52dfdeaaccb46e1121859ab1ea7c
ramram222 <kim.ram454@gmail.com>
2023-01-14 02:57:50 +0900

    update: [김가람] 하얀색 카트 상단 앱 바 추가 및 수정 적용 [ZTZ-APP-UI16]

--- ztz_app/lib/components/layout/menu_app_bar.dart
+++ ztz_app/lib/components/layout/menu_app_bar.dart
@@ -1,30 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
+import 'package:ztz_app/pages/order/cart.dart';
 import 'package:ztz_app/utility/text_styles.dart';
 
-import '../../pages/cart.dart';
-
-class MenuAppBar extends StatefulWidget with PreferredSizeWidget {
+class MenuAppBar extends StatelessWidget with PreferredSizeWidget {
   const MenuAppBar({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
   @override
-  State<MenuAppBar> createState() => _MenuAppBarState();
-
-  @override
   Size get preferredSize => Size.fromHeight(50);
-}
 
-// 초록색 앱 바 + 하얀색 글자 + 장바구니 버튼 있음
-class _MenuAppBarState extends State<MenuAppBar> {
   @override
+  // 초록색 앱 바 + 하얀색 글자 + 장바구니 버튼 있음
   Widget build(BuildContext context) {
     return AppBar(
       elevation: 0.1,
       // title => 카테고리, 검색, 마이페이지 등
-      title: Text(widget.title, style: whiteBoldTextStyle(16)),
+      title: Text(title, style: whiteBoldTextStyle(16)),
       centerTitle: true,
       actions: <Widget>[
         IconButton(
@@ -37,5 +31,8 @@ class _MenuAppBarState extends State<MenuAppBar> {
       ],
       backgroundColor: Color(0xff205C37),
     );
+
   }
 }
+
+
