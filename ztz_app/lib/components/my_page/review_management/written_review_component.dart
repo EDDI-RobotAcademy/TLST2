888bb4a7f67a1ebeb37144ca86df92951476866f
choi jaemin <foertonw5@naver.com>
2023-01-14 04:08:47 +0900

    feat : [최재민] 로그인한 회원이 작성한 리뷰 리스트를 확인 할 수있는 리뷰 컴포넌트[ZTZ-APP-UI70]

---
+++ ztz_app/lib/components/my_page/review_management/written_review_component.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import '../../../controller/account/sign_up_infos/account_state.dart';
+import '../../../controller/reivew/review_controller.dart';
+import '../../../controller/reivew/review_infos/review_info.dart';
+import '../../../utility/colors.dart';
+import '../../reivew/reivew_card.dart';
+
+class WrittenReviewComponent extends StatefulWidget {
+  const WrittenReviewComponent({Key? key}) : super(key: key);
+
+  @override
+  State<WrittenReviewComponent> createState() => _WrittenReviewComponent();
+}
+
+class _WrittenReviewComponent extends State<WrittenReviewComponent> {
+  var numberFormat = NumberFormat('###,###,###,###');
+  bool isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    setMemberReview();
+  }
+
+  void setMemberReview() async {
+    await ReviewController()
+        .requestMyPageReviewToSpring(AccountState.memberInfo['id']);
+    setState(() {
+      isLoading = true;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return !isLoading
+        ? const Padding(
+            // 로딩시 나오는 동그라미 동글동글
+            padding: EdgeInsets.all(100),
+            child: Center(
+              child: CircularProgressIndicator(
+                color: ColorStyle.mainColor,
+              ),
+            ),
+          )
+        : Container(
+            padding: EdgeInsets.all(15),
+            child: Column(
+              children: [
+                Container(
+                  padding: EdgeInsets.only(left: 15, bottom: 10),
+                  width: size.width,
+                  child: Text(
+                    "총 " +
+                        numberFormat.format(ReviewInfo.memberReviews.length) +
+                        "개의 리뷰",
+                    textAlign: TextAlign.left,
+                  ),
+                ),
+                const Divider(thickness: 1),
+                Column(
+                    children: List.generate(
+                        ReviewInfo.memberReviews.length,
+                        (index) => Padding(
+                              padding: EdgeInsets.zero,
+                              child: ReviewCard(
+                                username: ReviewInfo.memberReviews[index]['member']['username'],
+                                content: ReviewInfo.memberReviews[index]['content'],
+                                productName: ReviewInfo.memberReviews[index]['product']['name'],
+                                regDate: ReviewInfo.memberReviews[index]['regDate'],
+                                thumbnailFileName: ReviewInfo.memberReviews[index]['thumbnailFileName'] ??
+                                    "NoImage",
+                                rate: 4.0,
+                                isWrittenByBuyer: true,
+                                deleteReview: () {
+                                  ReviewController().requestDeleteReview(
+                                      ReviewInfo.memberReviews[index]
+                                          ['reviewNo']);
+                                },
+                                productId: ReviewInfo.memberReviews[index]
+                                    ['product']['productNo'],
+                                reviewNo: ReviewInfo.memberReviews[index]
+                                    ['reviewNo'],
+                                orderId: ReviewInfo.memberReviews[index]
+                                    ['orderInfo']['orderID'],
+                              ),
+                            ))),
+              ],
+            ),
+          );
+  }
+}
--- ztz_app/lib/components/reivew/reivew_card.dart
+++ ztz_app/lib/components/reivew/reivew_card.dart
@@ -3,23 +3,32 @@ import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
+import 'package:ztz_app/pages/reivew/review_modify_page.dart';
 
 import '../../pages/reivew/photo_review_page.dart';
 import '../../utility/text_styles.dart';
 
 class ReviewCard extends StatelessWidget {
-  ReviewCard({
-    Key? key,
-    required this.content,
-    required this.thumbnailFileName,
-    required this.regDate,
-    required this.username,
-    required this.productName,
-    required this.rate,
-  }) : super(key: key);
+  ReviewCard(
+      {Key? key,
+      required this.content,
+      required this.thumbnailFileName,
+      required this.regDate,
+      required this.username,
+      required this.productName,
+      required this.rate,
+      required this.isWrittenByBuyer,
+      required this.deleteReview,
+      required this.reviewNo,
+      required this.orderId,
+      required this.productId})
+      : super(key: key);
 
   final String content, thumbnailFileName, regDate, username, productName;
   final double rate;
+  final int productId, reviewNo , orderId;
+  final bool isWrittenByBuyer;
+  final VoidCallback deleteReview;
 
   @override
   Widget build(BuildContext context) {
@@ -55,33 +64,38 @@ class ReviewCard extends StatelessWidget {
           Container(
             padding: EdgeInsets.only(top: 5, bottom: 5),
             width: size.width,
-            child: thumbnailFileName != "NoImage"
+            child: thumbnailFileName == "NoImage"
                 ? SizedBox(
-              width: 0,
-            )
+                    width: 0,
+                  )
                 : Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  IconButton(
-                    onPressed: () {
-                      Get.to(
-                          PhotoReviewPage(
-                            rate: rate,
-                            productName: productName,
-                            username: username,
-                            content: content,
-                            regDate: regDate,
-                            thumbnailFileName: thumbnailFileName,
-                          ), //next page class
-                          duration: Duration(milliseconds: 500), //duration of transitions, default 1 sec
-                          transition: Transition.rightToLeft,//transition effect
-                          popGesture : true // 슬라이드로 뒤로가기
-                      );
-                    },
-                    icon: Image.asset("assets/images/item2.jpg"),iconSize: 80,)
-                ]),
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                        IconButton(
+                          onPressed: () {
+                            Get.to(
+                                PhotoReviewPage(
+                                  rate: rate,
+                                  productName: productName,
+                                  username: username,
+                                  content: content,
+                                  regDate: regDate,
+                                  thumbnailFileName: thumbnailFileName,
+                                ), //next page class
+                                duration: Duration(milliseconds: 500),
+                                //duration of transitions, default 1 sec
+                                transition: Transition.rightToLeft,
+                                //transition effect
+                                popGesture: true // 슬라이드로 뒤로가기
+                                );
+                          },
+                          icon: Image.asset(
+                              "assets/images/uploadImg/$thumbnailFileName"),
+                          iconSize: 80,
+                        )
+                      ]),
           ),
           Container(
               padding: EdgeInsets.only(left: 10, top: 10),
@@ -102,6 +116,21 @@ class ReviewCard extends StatelessWidget {
               textAlign: TextAlign.left,
               style: mediumGrayTextStyle(),
             ),
+          ),
+          Container(
+            child: isWrittenByBuyer ? Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                TextButton(onPressed: () {
+                  Get.to(() => ReviewModifyPage(productName: productName, productId: productId, reviewNo: reviewNo, orderId: orderId,));
+                }, child: Text("수정" ,style: blackBoldTextStyle(10),)),
+                TextButton(onPressed: () {
+                  showCheckPassword(context);
+
+                }, child: Text("삭제", style: blackBoldTextStyle(10))),
+              ],
+            ) : null,
           ),
           const Divider(thickness: 1),
         ],
@@ -126,4 +155,31 @@ class ReviewCard extends StatelessWidget {
       onRatingUpdate: (rating) {},
     );
   }
+
+  void showCheckPassword(context){
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        // return object of type Dialog
+        return AlertDialog(
+          title: new Text("정말로 삭제하시겠습니까?"),
+          actions: <Widget>[
+            FlatButton(
+              child: const Text("삭제"),
+              onPressed: () {
+                deleteReview();
+              },
+            ),
+            FlatButton(
+              child: const Text("취소"),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
 }
