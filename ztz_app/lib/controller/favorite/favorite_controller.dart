9c7f38730ee94aea1bf6171936765e8a1863fbca
dobbymun <tearofcake@gmail.com>
2023-01-15 01:18:16 +0900

    feat:[박진주]로그인 회원이 상품 찜버튼을 클릭하여 저장하고 찜여부를 조회하는 UI[ZTZ-APP-UI73]

---
+++ ztz_app/lib/controller/favorite/favorite_controller.dart
@@ -0,0 +1,33 @@
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
+
+import 'favorite_infos/favorite_info.dart';
+
+class FavoriteController extends GetxController{
+  static const httpUri = '192.168.200.168:7777';
+
+  requestFavoriteStatusToSpring(memberId, productNo, favoriteType) async{
+    
+    var data = {'memberId' : memberId, 'productNo' : productNo, 'favoriteType' : favoriteType};
+    var body = json.encode(data);
+    try{
+      var requestFavoriteResponse = await http.post(
+        Uri.http(httpUri,'ztz/products/favorite/changeFavoriteStatus'),
+        headers: {"Content-Type": "application/json"},
+        body: body,
+      );
+
+      debugPrint(requestFavoriteResponse.statusCode.toString());
+      if(requestFavoriteResponse.statusCode == 200){
+        debugPrint("좋아요 결과 : " + utf8.decode(requestFavoriteResponse.bodyBytes).toString());
+        FavoriteInfo.favoriteStatus = jsonDecode(utf8.decode(requestFavoriteResponse.bodyBytes));
+      }
+    }catch(e){
+      debugPrint("오류 발생 " + e.toString());
+    }
+  }
+}
--- ztz_app/lib/pages/product/product_detail_page.dart
+++ ztz_app/lib/pages/product/product_detail_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:like_button/like_button.dart';
+import 'package:ztz_app/controller/favorite/favorite_controller.dart';
 import 'package:ztz_app/controller/product/product_infos/product_info.dart';
 import 'package:ztz_app/controller/reivew/review_infos/review_info.dart';
 import 'package:ztz_app/utility/colors.dart';
@@ -8,6 +9,8 @@ import '../../components/product/product_detail/prdouct_buy_modal_component.dart
 import '../../components/product/product_detail/product_detail_appbar.dart';
 import '../../components/product/product_detail/product_detail_component.dart';
 import '../../components/reivew/product_review_component.dart';
+import '../../controller/account/sign_up_infos/account_state.dart';
+import '../../controller/favorite/favorite_infos/favorite_info.dart';
 import '../../controller/product/product_infos/product_info.dart';
 import '../../utility/button_style.dart';
 import '../../utility/text_styles.dart';
@@ -20,90 +23,165 @@ class ProductDetailPage extends StatefulWidget{
 }
 class _ProductDetailPage extends State<ProductDetailPage>{
 
+  var isLoading = false;
+
   @override
   void initState() {
     callProductInfo();
+    callFavoriteNumInfo();
     super.initState();
   }
   void callProductInfo() async {
     ProductInfo().setProductDetailINfo();
     ReviewInfo().setReviewAve();
   }
+
+  void callFavoriteNumInfo() async{
+    await FavoriteController().requestFavoriteStatusToSpring(
+        AccountState.memberInfo['id'],
+        ProductInfo.productNo,
+        "favoriteCheck");
+    FavoriteInfo().setFavoriteInfo();
+    setState(() {
+      isLoading = true;
+      });
+    debugPrint("좋아요 조회함 상품번호:" + ProductInfo.productNo.toString());
+    debugPrint("좋아요 조회함 좋아요수는?:" + FavoriteInfo.favoriteNum.toString());
+    debugPrint("좋아요 조회함 플래그는?:" + FavoriteInfo.productFavoriteFlag.toString());
+  }
+
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-          appBar: ProductDetailAppbar(reviewCnt: ReviewInfo.reviewCnt, productName: ProductInfo.productName,),
-          body: TabBarView(
-            children: [
-              SafeArea(
-                  child: SingleChildScrollView(
-                    child: ProductDetailComponent(productNo: widget.productNo),
-                  )
-              ),
-              SafeArea(
-                  child: SingleChildScrollView(
-                    child: ProductReviewComponent()
-                  )
-              ),
-            ],
+
+    if (!isLoading) { // 로딩 판단 유무
+      return const Padding( // 로딩시 나오는 동그라미 동글동글
+        padding: EdgeInsets.all(100),
+        child: Center(
+          child: CircularProgressIndicator(
+            color: ColorStyle.mainColor,
           ),
-        bottomNavigationBar: BottomAppBar(
-          child: Container(
-            padding: EdgeInsets.all(10),
-            child: Row(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
+        ),
+      );
+    } else{
+      return DefaultTabController(
+        length: 2,
+        child: Scaffold(
+            appBar: ProductDetailAppbar(reviewCnt: ReviewInfo.reviewCnt, productName: ProductInfo.productName,),
+            body: TabBarView(
               children: [
-                Container(
-                  child: Container(
-                    width: 50,
-                    height: 50,
-                    padding: EdgeInsets.only(left: 2),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      border: Border.all(color: ColorStyle.textGray)
-                    ),
-                    child: LikeButton(
-                      likeBuilder: (bool isLiked) {
-                        return isLiked ?
-                        Icon(
-                          Icons.favorite,
-                          color:Colors.red,
-                        ):
-                        Icon(
-                          Icons.favorite_border,
-                          color:ColorStyle.mainColor,
-                        )
-                        ;
-                      },
-                    ),
-                  ),
+                SafeArea(
+                    child: SingleChildScrollView(
+                      child: ProductDetailComponent(productNo: widget.productNo),
+                    )
                 ),
-                Container(
-                  child: ElevatedButton(
-                      style:defaultElevatedButtonStyle(size.width - 80,50),
-                      onPressed: (){
-                        showModalBottomSheet(
-                            context: context,
-                            builder: (BuildContext context){
-                              return Container(
-                                  height: 370,
-                                  child: ProductBuyModalComponent()
-                              );
-                            }
-                        );
-                      },
-                      child: Text("구매하기",style: lageWhiteTextStyle(),)),
-                )
+                SafeArea(
+                    child: SingleChildScrollView(
+                        child: ProductReviewComponent()
+                    )
+                ),
               ],
             ),
-          ),
-        )
-      ),
+            bottomNavigationBar: BottomAppBar(
+              child: Container(
+                padding: EdgeInsets.all(10),
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Container(
+                      child: Container(
+                        width: 50,
+                        height: 50,
+                        padding: EdgeInsets.only(left: 2),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            border: Border.all(color: ColorStyle.textGray)
+                        ),
+                        child:
+                        LikeButton(
+                          onTap: onLikeButtonTapped,
+                          likeBuilder: (bool isLiked) {
+                            return FavoriteInfo.productFavoriteFlag ?
+                            Icon(
+                              Icons.favorite,
+                              color:Colors.red,
+                            ):
+                            Icon(
+                              Icons.favorite_border,
+                              color:ColorStyle.mainColor,
+                            );
+                          },
+                        ),
+                      ),
+                    ),
+                    Container(
+                      child: ElevatedButton(
+                          style:defaultElevatedButtonStyle(size.width - 80,50),
+                          onPressed: (){
+                            showModalBottomSheet(
+                                context: context,
+                                builder: (BuildContext context){
+                                  return Container(
+                                      height: 370,
+                                      child: ProductBuyModalComponent()
+                                  );
+                                }
+                            );
+                          },
+                          child: Text("구매하기",style: lageWhiteTextStyle(),)),
+                    )
+                  ],
+                ),
+              ),
+            )
+        ),
+      );
+    }
+
+  }
+
+  Future<bool> onLikeButtonTapped(bool isLiked) async{
+
+    await AccountState.accountGet.isLoginCheck();
+    if (AccountState.accountGet.isLogin.value) {
+      await FavoriteController().requestFavoriteStatusToSpring(
+          AccountState.memberInfo['id'],
+          ProductInfo.productNo,
+          "favoriteLike");
+      FavoriteInfo().setFavoriteInfo();
+      debugPrint("좋아요 클릭함 상품번호:" + ProductInfo.productNo.toString());
+      debugPrint("좋아요 클릭함! 좋아요 수는?:" + FavoriteInfo.favoriteStatus['favoriteNum'].toString());
+
+        return !isLiked;
+
+    } else{
+      //위젯 띄워주기
+      debugPrint("로그인 상태만 좋아요 가능!");
+      showFailFavorite();
+      return isLiked;
+    }
+  }
+
+  void showFailFavorite(){
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        // return object of type Dialog
+        return AlertDialog(
+          title: new Text("로그인 시 가능한 서비스입니다."),
+          content: new Text("로그인 후 이용해주세요."),
+          actions: <Widget>[
+            FlatButton(
+              child: const Text("Close"),
+              //경로 이동 없이 그냥 닫기
+              onPressed: () => Navigator.pop(context),
+            ),
+          ],
+        );
+      },
     );
-  } 
+  }
 
 }
