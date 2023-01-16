21335263f85f0142a8e62247bd8321da9b6abacd
choi jaemin <foertonw5@naver.com>
2023-01-16 18:00:18 +0900

    feat : [최재민] 마이페이지에서 개인정보 수정 버튼 클릭시 보이는 개인정보수정UI[ZTZ-APP-UI80]

---
+++ ztz_app/lib/components/my_page/personal_info_management/profile_modify_component.dart
@@ -0,0 +1,262 @@
+import 'package:flutter/material.dart';
+import 'package:ztz_app/controller/account/member_api.dart';
+import 'package:ztz_app/controller/account/member_modify_infos/modify_profile_info.dart';
+import 'package:ztz_app/utility/button_style.dart';
+import 'package:ztz_app/utility/text_field_decoration.dart';
+import 'package:ztz_app/utility/text_styles.dart';
+
+import '../../../controller/account/sign_up_infos/account_state.dart';
+import '../../../pages/home_page.dart';
+import '../../../utility/colors.dart';
+import '../../../utility/text_field_validate.dart';
+
+class ProfileModifyComponent extends StatefulWidget {
+  const ProfileModifyComponent({Key? key}) : super(key: key);
+
+  @override
+  State<ProfileModifyComponent> createState() => _ProfileModifyComponent();
+}
+
+class _ProfileModifyComponent extends State<ProfileModifyComponent> {
+
+  late bool managerCheck;
+  bool isLoading = false;
+  late TextEditingController passwordController;
+  late TextEditingController newPasswordController;
+  late TextEditingController newPasswordCheckController;
+  late TextEditingController phoneNumberController = TextEditingController( text: AccountState.memberProfile['phoneNumber']);
+  late TextEditingController managerCodeController;
+
+  late FocusNode passwordFocus;
+  late FocusNode newPasswordFocus;
+  late FocusNode newPasswordCheckFocus;
+  late FocusNode managerCodeFocus;
+  late FocusNode phoneNumberFocus;
+
+  @override
+  void initState() {
+    super.initState();
+    managerCheck = AccountState.memberInfo['managerCheck'];
+    passwordController = TextEditingController();
+    newPasswordController = TextEditingController();
+    newPasswordCheckController = TextEditingController();
+    managerCodeController = TextEditingController();
+
+    passwordFocus = FocusNode();
+    newPasswordFocus = FocusNode();
+    newPasswordCheckFocus = FocusNode();
+    phoneNumberFocus = FocusNode();
+    managerCodeFocus = FocusNode();
+    setState(() {
+      isLoading = true;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    if(!isLoading){
+      return Padding( // 로딩시 나오는 동그라미 동글동글
+        padding: EdgeInsets.all(100),
+        child: Center(
+          child: CircularProgressIndicator(
+            color: ColorStyle.mainColor,
+          ),
+        ),
+      );
+    } else{
+      return Container(
+        padding: EdgeInsets.all(15),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              child: managerCheck
+                  ? Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 8, bottom: 8),
+                    child: Text(
+                      "관리자 코드",
+                      style: blackTextStyle(14),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 50,
+                    child: TextFormField(
+                        controller: managerCodeController,
+                        decoration: textFieldDecoration("관리자 코드"),
+                        keyboardType: TextInputType.text,
+                        focusNode: managerCodeFocus,
+                        onSaved: (value) {
+                          setState(() {
+                            managerCodeController.text = value!;
+                          });
+                        }),
+                  )
+                ],
+              )
+                  : null,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 8, bottom: 8),
+              child: Text(
+                "현재 비밀번호",
+                style: blackTextStyle(14),
+              ),
+            ),
+            SizedBox(
+                height: 50,
+                child: passwordTextFromField(
+                    passwordController, "현재 비밀번호", passwordFocus)),
+            Padding(
+              padding: const EdgeInsets.only(top: 8, bottom: 8),
+              child: Text("새 비밀번호", style: blackTextStyle(14)),
+            ),
+            SizedBox(
+                height: 50,
+                child: passwordTextFromField(
+                    newPasswordController, "새 비밀번호", newPasswordFocus)),
+            Padding(
+              padding: const EdgeInsets.only(top: 8, bottom: 8),
+              child: Text("새 비밀번호 확인", style: blackTextStyle(14)),
+            ),
+            SizedBox(
+                height: 50,
+                child: passwordTextFromField(newPasswordCheckController,
+                    "새 비밀번호 확인", newPasswordCheckFocus)),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text("전화번호", style: blackTextStyle(14)),
+            ),
+            SizedBox(
+              height: 50,
+              child: TextFormField(
+                  controller: phoneNumberController,
+                  decoration: textFieldDecoration("전화번호"),
+                  keyboardType: TextInputType.text,
+                  focusNode: phoneNumberFocus,
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  onSaved: (value) {
+                    setState(() {
+                      phoneNumberController.text = value!;
+                    });
+                  }),
+            ),
+            ElevatedButton(
+                style: defaultElevatedButtonStyle(size.width - 10, 20),
+                onPressed: () {
+                  String myPassword = newPasswordController.text;
+                  int tmp = myPassword.compareTo(newPasswordCheckController.text);
+                  debugPrint("myPassword : " +
+                      myPassword +
+                      "newPasswordCheckController.text :" +
+                      newPasswordCheckController.text +
+                      "tmp : " +
+                      tmp.toString());
+
+                  if (tmp != 0) {
+                    showCheckPassword();
+                  } else if (managerCheck) {
+                    modifyManagerProfile();
+                  } else {
+                    modifyMemberProfile();
+                  }
+                },
+                child: Text("변경하기"))
+          ],
+        ),
+      );
+    }
+
+  }
+
+  Widget passwordTextFromField(controller, hintText, focusNode) {
+    return TextFormField(
+      controller: controller,
+      decoration: textFieldDecoration(hintText),
+      keyboardType: TextInputType.visiblePassword,
+      focusNode: focusNode,
+      obscureText: true,
+      obscuringCharacter: '*',
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      validator: (value) => CheckValidate().validatePassword(focusNode, value!),
+      onSaved: (value) {
+        setState(() {
+          controller.text = value!;
+        });
+      },
+    );
+  }
+
+  void modifyMemberProfile() async {
+    ModifyProfileInfo modifyProfileInfo = ModifyProfileInfo(
+        passwordController.text,
+        newPasswordController.text,
+        phoneNumberController.text,
+        AccountState.memberInfo['id'],
+        managerCodeController.text);
+
+    await MemberApi().requestModifyProfileToSpring(modifyProfileInfo);
+
+    showModifyResultPassword();
+  }
+
+  void modifyManagerProfile() async {
+    ModifyProfileInfo modifyProfileInfo = ModifyProfileInfo(
+        passwordController.text,
+        newPasswordController.text,
+        phoneNumberController.text,
+        AccountState.memberInfo['id'],
+        managerCodeController.text);
+
+    await MemberApi().requestModifyProfileToSpring(modifyProfileInfo);
+
+    showModifyResultPassword();
+  }
+
+  void showModifyResultPassword() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        // return object of type Dialog
+        return AlertDialog(
+          title: const Text("결과"),
+          content: Text(ModifyProfileInfo.resultMsg),
+          actions: <Widget>[
+            FlatButton(
+              child: const Text("확인"),
+              onPressed: () {
+                Navigator.of(context).pushAndRemoveUntil(
+                    MaterialPageRoute(builder: (context) => HomePage()),
+                    (route) => false);
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void showCheckPassword() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        // return object of type Dialog
+        return AlertDialog(
+          title: const Text("비밀번호가 다릅니다"),
+          content: const Text("비밀번호를 확인해주세요"),
+          actions: <Widget>[
+            FlatButton(
+              child: const Text("Close"),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
--- ztz_app/lib/controller/account/member_api.dart
+++ ztz_app/lib/controller/account/member_api.dart
@@ -6,8 +6,10 @@ import 'package:http/http.dart' as http;
 import 'package:ztz_app/controller/account/member_modify_infos/modify_address_info.dart';
 import 'package:ztz_app/controller/account/sign_up_infos/account_state.dart';
 
+import 'member_modify_infos/modify_profile_info.dart';
+
 class MemberApi{
-  static const httpUri = '172.30.1.54:7777';
+  static const httpUri = '192.168.0.8:7777';
 
   userVerification(token) async{
     var data = {'token': token};
@@ -81,6 +83,35 @@ class MemberApi{
       }
     } catch(e) {
       debugPrint("주소 수정 오류 발생: " + e.toString());
+    }
+  }
+
+  requestModifyProfileToSpring(ModifyProfileInfo modifyProfileInfo) async {
+    var data = {
+      'password' : modifyProfileInfo.password,
+      'manager_code' : modifyProfileInfo.manager_code,
+      'new_password' : modifyProfileInfo.new_password,
+      'phoneNumber' : modifyProfileInfo.phoneNumber,
+      'id' : modifyProfileInfo.id
+    };
+    var body = json.encode(data);
+
+    try {
+      var modifyAddressResponse = await http.post(
+          Uri.http(httpUri, 'ztz/member/modify-profile'),
+          headers: {"Content-Type": "application/json"},
+          body: body
+      );
+      if(modifyAddressResponse.statusCode == 200) {
+        debugPrint("회원 정보 수정 결과: " + utf8.decode(modifyAddressResponse.bodyBytes).toString());
+        String tmpResult = utf8.decode(modifyAddressResponse.bodyBytes).toString();
+        ModifyProfileInfo.resultMsg = tmpResult;
+      }else{
+        debugPrint("통신 오류: " + modifyAddressResponse.statusCode.toString());
+        ModifyProfileInfo.resultMsg = "통신 오류로 인해 회원 정보 변경이 실패 했습니다.\n 잠시뒤에 다시 시도해주세요.";
+      }
+    } catch(e) {
+      debugPrint("오류 발생: " + e.toString());
     }
   }
 }
