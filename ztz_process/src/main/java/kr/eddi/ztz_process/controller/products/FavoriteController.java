b8124945a84ea6f070d72d9ca25112c5a807eefb
dobbymun <tearofcake@gmail.com>
2023-01-17 19:02:57 +0900

    feat:[박진주]로그인 회원의 마이 찜리스트 조회시스템[ZTZ-PROCESS82]

--- ztz_process/src/main/java/kr/eddi/ztz_process/controller/products/FavoriteController.java
+++ ztz_process/src/main/java/kr/eddi/ztz_process/controller/products/FavoriteController.java
@@ -1,11 +1,15 @@
 package kr.eddi.ztz_process.controller.products;
 
+import kr.eddi.ztz_process.controller.member.form.MemberLoggedInTokenForm;
 import kr.eddi.ztz_process.controller.products.request.FavoriteInfo;
 import kr.eddi.ztz_process.controller.products.response.FavoriteResponse;
+import kr.eddi.ztz_process.entity.products.Favorite;
 import kr.eddi.ztz_process.service.products.FavoriteService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -24,5 +28,12 @@ public class FavoriteController {
         String favoriteType = favoriteInfo.getFavoriteType();
 
         return service.favoriteStatus(memberId, boardNo, favoriteType);
+    }
+
+    @PostMapping("/myFavorite")
+    public List<Favorite> myFavoriteProductList (@RequestBody MemberLoggedInTokenForm memberLoggedInTokenForm) {
+        log.info("찜한 상품- 멤버 아이디: "+ memberLoggedInTokenForm.getToken());
+
+        return service.myFavoriteProductList(memberLoggedInTokenForm.getToken());
     }
 }
--- ztz_process/src/main/java/kr/eddi/ztz_process/service/products/FavoriteService.java
+++ ztz_process/src/main/java/kr/eddi/ztz_process/service/products/FavoriteService.java
@@ -1,7 +1,15 @@
 package kr.eddi.ztz_process.service.products;
 
 import kr.eddi.ztz_process.controller.products.response.FavoriteResponse;
+import kr.eddi.ztz_process.entity.products.Favorite;
+
+import java.util.List;
 
 public interface FavoriteService {
     public FavoriteResponse favoriteStatus(Long memberId, Long productNo, String favoriteType);
+
+    public List<Favorite> myFavoriteProductList(String token);
+
+
+
 }
