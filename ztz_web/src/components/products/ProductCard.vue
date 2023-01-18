f9c1f4568e14bd48ead2ecc088fc5b8fe83b57d1
tonowoods <tono.woods@gmail.com>
2023-01-18 22:09:17 +0900

    style: [임보경] 상품 리스트 썸네일 및 폰트 크기, 스타일 변경 [ZTZ-WEB-UI112]

--- ztz_web/src/components/products/ProductCard.vue
+++ ztz_web/src/components/products/ProductCard.vue
@@ -1,19 +1,24 @@
 <template>
-<div class = "productCard">
-
-  <div class="productInfo">
-    <div class="thumb">
-      <img :src="require(`../../assets/products/uploadImg/${product.productInfo.thumbnailFileName}`)" >
-    </div>
-    <p class="brand" >{{ product.brand }}</p>
-    <p class="productName" >{{ product.name }}</p>
-    <div class="price" >
-      <p><span v-if="product.monthAlcoholCheck" style="color: red" >10% &nbsp; </span>{{ product.price | numberFormat }}</p>
-      <p>won</p>
+  <div class="productCard">
+    <div class="productInfo">
+      <div class="thumb">
+        <img
+          :src="
+            require(`../../assets/products/uploadImg/${product.productInfo.thumbnailFileName}`)
+          "
+        />
+      </div>
+      <p class="brand">{{ product.brand }}</p>
+      <p class="productName">{{ product.name }}</p>
+      <div class="price">
+        <p>
+          <span v-if="product.monthAlcoholCheck" style="color: red"
+            >10% &nbsp; </span
+          >{{ product.price | numberFormat }}원
+        </p>
+      </div>
     </div>
   </div>
-
-</div>
 </template>
 
 <script>
@@ -23,7 +28,7 @@ export default {
     product: {
       type: Object,
       required: true,
-    }
-  }
-}
+    },
+  },
+};
 </script>
