c7ef3a1f3f4fbc2410ed6c47342191cf46047173
ramram222 <kim.ram454@gmail.com>
2023-01-20 04:19:47 +0900

    style: [김가람] 로그인 사용자가 마이페이지에서 보는 1:1문의 UI 코드 수정[ZTZ-WEB-UI113]

--- ztz_web/src/components/mypage/MyPageQuestionForm.vue
+++ ztz_web/src/components/mypage/MyPageQuestionForm.vue
@@ -13,12 +13,14 @@
           <div class="question-board">
             <div class="question">
               <v-row>
-              <p class="mt-3 mb-5" style="color: gray">{{ item.boardCategory.categoryType == "ORDER_PAYMENT"? "주문/결제문의"
-                  : item.boardCategory.categoryType == "PRODUCT"? "상품문의"
-                      : item.boardCategory.categoryType == "DELIVERY"? "배송문의"
-                          : item.boardCategory.categoryType == "RETURN_CHANGE"? "반품/교환문의"
-                              : item.boardCategory.categoryType == "TOUR"? "양조장문의"
-                                  : item.boardCategory.categoryType == "OTHER"? "기타문의" : "카테고리오류"}}</p>
+                <p class="mt-3 mb-5" style="color: gray">{{
+                    item.boardCategory.categoryType == "ORDER_PAYMENT" ? "주문/결제문의"
+                        : item.boardCategory.categoryType == "PRODUCT" ? "상품문의"
+                            : item.boardCategory.categoryType == "DELIVERY" ? "배송문의"
+                                : item.boardCategory.categoryType == "RETURN_CHANGE" ? "반품/교환문의"
+                                    : item.boardCategory.categoryType == "TOUR" ? "양조장문의"
+                                        : item.boardCategory.categoryType == "OTHER" ? "기타문의" : "카테고리오류"
+                  }}</p>
               </v-row>
               <v-row>
                 <v-img
@@ -72,6 +74,8 @@ export default {
         },
         {text: '제목', value: 'title'},
         {text: '작성일', value: 'createDateTime', width: 200},
+        // eslint-disable-next-line no-constant-condition
+        {text: '답변상태', value: 'answerState'},
         {text: '', value: 'data-table-expand'},
       ],
       showRegisterBoard: false,
@@ -84,7 +88,7 @@ export default {
     ]),
     btnRegisterBoard() {
       this.showRegisterBoard = true
-    }
+    },
   },
   async mounted() {
     this.token = window.localStorage.getItem("userInfo");
@@ -104,9 +108,11 @@ export default {
   width: 1000px;
   margin: 50px auto;
 }
+
 .question-board {
   margin: 30px;
 }
+
 .comment {
   margin-top: 50px;
   margin-bottom: 50px;
