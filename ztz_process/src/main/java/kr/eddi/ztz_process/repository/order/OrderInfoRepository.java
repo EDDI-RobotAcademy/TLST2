852f42afb833a97814b3321f6b44f78a78f0c1dd
dobbymun <tearofcake@gmail.com>
2023-01-20 00:24:11 +0900

    fix:[박진주]관리자 회원의 매출조회 시스템[ZTZ-PROCESS57]

--- ztz_process/src/main/java/kr/eddi/ztz_process/repository/order/OrderInfoRepository.java
+++ ztz_process/src/main/java/kr/eddi/ztz_process/repository/order/OrderInfoRepository.java
@@ -20,9 +20,16 @@ public interface OrderInfoRepository extends JpaRepository<OrderInfo, Long> {
     @Query("select o from OrderInfo o join fetch o.payment join fetch o.member join fetch o.product where o.payment.paymentId = :paymentId")
     List<OrderInfo> findByPaymentId(Long paymentId);
 
-    @Query("select o from OrderInfo o where o.orderState = kr.eddi.ztz_process.entity.order.PaymentState.PAYMENT_CONFIRM ")
+    @Query("select o from OrderInfo o where " +
+            "o.orderState = kr.eddi.ztz_process.entity.order.PaymentState.PAYMENT_CONFIRM or " +
+            "o.orderState = kr.eddi.ztz_process.entity.order.PaymentState.WRITE_REVIEW or " +
+            "o.orderState = kr.eddi.ztz_process.entity.order.PaymentState.PART_WRITE_REVIEW  or " +
+            "o.orderState = kr.eddi.ztz_process.entity.order.PaymentState.ABLE_WRITE_REVIEW ")
     List<OrderInfo> findSalesList();
 
     @Query("select o from OrderInfo o join fetch o.member join fetch o.product join fetch o.payment where (o.member.id =:memberId) and (o.orderState =:PAYMENT_CONFIRM or o.orderState =:REFUND_REQUEST)")
     List<OrderInfo> findByPaymentState(PaymentState PAYMENT_CONFIRM,PaymentState REFUND_REQUEST, Long memberId);
+
+    @Query("select o from OrderInfo o join fetch o.payment join fetch o.member join fetch o.product op where op.productNo = :productNo")
+    List<OrderInfo> findByProductNo(Long productNo);
 }
